b0fbe089629a69d014af1d206379d43d5a04be16
metalicn20 <metalicn20@gmail.com>
2021-08-07 14:24:40 +0430

    Add authentication to swagger

--- Api/api-app/src/main/kotlin/co/nilin/mixchange/app/ApiApp.kt
+++ Api/api-app/src/main/kotlin/co/nilin/mixchange/app/ApiApp.kt
@@ -4,12 +4,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
-import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.*
 import springfox.documentation.builders.PathSelectors.regex
-import springfox.documentation.service.ApiInfo
+import springfox.documentation.service.*
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger.web.SecurityConfiguration
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder
 import springfox.documentation.swagger2.annotations.EnableSwagger2
+import java.util.Collections.singletonList
 
 
 @SpringBootApplication
@@ -24,6 +28,18 @@ class ApiApp {
             .select()
             .paths(regex("^/api/v3.*"))
             .build()
+            .globalRequestParameters(
+                singletonList(
+                    RequestParameterBuilder()
+                        .name("content-type")
+                        .description("content-type")
+                        .`in`(ParameterType.HEADER)
+                        .required(true)
+                        .build()
+                )
+            )
+            .securitySchemes(singletonList(oauth()))
+            .securityContexts(singletonList(securityContext()))
     }
 
     private fun apiInfo(): ApiInfo? {
@@ -33,6 +49,47 @@ class ApiApp {
             .license("MIT License")
             .licenseUrl("https://github.com/opexdev/Back-end/blob/feature/1-MVP/LICENSE")
             .version("0.1")
+            .build()
+    }
+
+    @Bean
+    fun oauth(): SecurityScheme? {
+        return OAuthBuilder()
+            .name("opex")
+            .grantTypes(grantTypes())
+            .scopes(scopes())
+            .build()
+    }
+
+    fun scopes(): List<AuthorizationScope?>? {
+        return emptyList()
+    }
+
+    fun grantTypes(): List<GrantType?>? {
+        val tokenUrl = "http://localhost:8083/auth/realms/mixchange/protocol/openid-connect/token"
+        val grantType = ResourceOwnerPasswordCredentialsGrant(tokenUrl)
+        return singletonList(grantType)
+    }
+
+    @Bean
+    fun securityContext(): SecurityContext? {
+        val securityReference = SecurityReference.builder()
+            .reference("opex")
+            .scopes(emptyArray())
+            .build()
+        return SecurityContext.builder()
+            .securityReferences(singletonList(securityReference))
+            .operationSelector { true }
+            .build()
+    }
+
+    @Bean
+    fun securityInfo(): SecurityConfiguration? {
+        return SecurityConfigurationBuilder.builder()
+            .clientId("admin-cli")
+            .realm("mixchange")
+            .appName("opex")
+            .scopeSeparator(",")
             .build()
     }
 }
--- MatchingGateway/gateway-app/src/main/kotlin/co/nilin/mixchange/app/MatchingGatewayApp.kt
+++ MatchingGateway/gateway-app/src/main/kotlin/co/nilin/mixchange/app/MatchingGatewayApp.kt
@@ -5,10 +5,17 @@ import org.springframework.boot.runApplication
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.ComponentScan
 import springfox.documentation.builders.ApiInfoBuilder
+import springfox.documentation.builders.OAuthBuilder
 import springfox.documentation.builders.PathSelectors.regex
-import springfox.documentation.service.ApiInfo
+import springfox.documentation.builders.RequestParameterBuilder
+import springfox.documentation.service.*
 import springfox.documentation.spi.DocumentationType
+import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
+import springfox.documentation.swagger.web.SecurityConfiguration
+import springfox.documentation.swagger.web.SecurityConfigurationBuilder
+import java.util.*
+import java.util.Collections.singletonList
 
 @SpringBootApplication
 @ComponentScan("co.nilin.mixchange")
@@ -21,6 +28,18 @@ class MatchingGatewayApp {
             .select()
             .paths(regex("^/actuator.*").negate())
             .build()
+            .globalRequestParameters(
+                singletonList(
+                    RequestParameterBuilder()
+                        .name("content-type")
+                        .description("content-type")
+                        .`in`(ParameterType.HEADER)
+                        .required(true)
+                        .build()
+                )
+            )
+            .securitySchemes(singletonList(oauth()))
+            .securityContexts(singletonList(securityContext()))
     }
 
     private fun apiInfo(): ApiInfo? {
@@ -30,6 +49,47 @@ class MatchingGatewayApp {
             .license("MIT License")
             .licenseUrl("https://github.com/opexdev/Back-end/blob/feature/1-MVP/LICENSE")
             .version("0.1")
+            .build()
+    }
+
+    @Bean
+    fun oauth(): SecurityScheme? {
+        return OAuthBuilder()
+            .name("opex")
+            .grantTypes(grantTypes())
+            .scopes(scopes())
+            .build()
+    }
+
+    fun scopes(): List<AuthorizationScope?>? {
+        return emptyList()
+    }
+
+    fun grantTypes(): List<GrantType?>? {
+        val tokenUrl = "http://localhost:8083/auth/realms/mixchange/protocol/openid-connect/token"
+        val grantType = ResourceOwnerPasswordCredentialsGrant(tokenUrl)
+        return singletonList(grantType)
+    }
+
+    @Bean
+    fun securityContext(): SecurityContext? {
+        val securityReference = SecurityReference.builder()
+            .reference("opex")
+            .scopes(emptyArray())
+            .build()
+        return SecurityContext.builder()
+            .securityReferences(singletonList(securityReference))
+            .operationSelector { true }
+            .build()
+    }
+
+    @Bean
+    fun securityInfo(): SecurityConfiguration? {
+        return SecurityConfigurationBuilder.builder()
+            .clientId("admin-cli")
+            .realm("mixchange")
+            .appName("opex")
+            .scopeSeparator(",")
             .build()
     }
 }
