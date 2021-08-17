4e29389cd875a83e0c277a8c37679566e4f7f664
metalicn20 <metalicn20@gmail.com>
2021-08-17 12:47:00 +0430

    Close #14, Extract swagger config (#24)
    * Extract swagger config from the application class
    
    * Fix API application class
    
    * Fix swagger class definition

--- Wallet/wallet-app/src/main/kotlin/co/nilin/opex/wallet/app/WalletApp.kt
+++ Wallet/wallet-app/src/main/kotlin/co/nilin/opex/wallet/app/WalletApp.kt
@@ -16,92 +16,14 @@ import springfox.documentation.spi.service.contexts.SecurityContext
 import springfox.documentation.spring.web.plugins.Docket
 import springfox.documentation.swagger.web.SecurityConfiguration
 import springfox.documentation.swagger.web.SecurityConfigurationBuilder
+import springfox.documentation.swagger2.annotations.EnableSwagger2
 import java.security.Principal
 import java.util.Collections.singletonList
 
 @SpringBootApplication
 @ComponentScan("co.nilin.opex")
-class WalletApp {
-    @Value("\${swagger.authUrl}")
-    val authUrl: String = ""
-
-    @Bean
-    fun opexWallet(): Docket? {
-        return Docket(DocumentationType.SWAGGER_2)
-            .groupName("opex-wallet")
-            .apiInfo(apiInfo())
-            .select()
-            .paths(regex("^/actuator.*").negate())
-            .build()
-            .globalRequestParameters(
-                singletonList(
-                    RequestParameterBuilder()
-                        .name("content-type")
-                        .description("content-type")
-                        .`in`(ParameterType.HEADER)
-                        .required(true)
-                        .build()
-                )
-            )
-            .ignoredParameterTypes(AuthenticationPrincipal::class.java, Principal::class.java)
-            .useDefaultResponseMessages(false)
-            .securitySchemes(singletonList(oauth()))
-            .securityContexts(singletonList(securityContext()))
-    }
-
-    private fun apiInfo(): ApiInfo? {
-        return ApiInfoBuilder()
-            .title("OPEX API")
-            .description("Backend for opex exchange.")
-            .license("MIT License")
-            .licenseUrl("https://github.com/opexdev/Back-end/blob/feature/1-MVP/LICENSE")
-            .version("0.1")
-            .build()
-    }
-
-    @Bean
-    fun oauth(): SecurityScheme? {
-        return OAuthBuilder()
-            .name("opex")
-            .grantTypes(grantTypes())
-            .scopes(scopes())
-            .build()
-    }
-
-    fun scopes(): List<AuthorizationScope?>? {
-        return emptyList()
-    }
-
-    fun grantTypes(): List<GrantType?>? {
-        val tokenUrl = "${authUrl}/auth/realms/mixchange/protocol/openid-connect/token"
-        val grantType = ResourceOwnerPasswordCredentialsGrant(tokenUrl)
-        return singletonList(grantType)
-    }
-
-    @Bean
-    fun securityContext(): SecurityContext? {
-        val securityReference = SecurityReference.builder()
-            .reference("opex")
-            .scopes(emptyArray())
-            .build()
-        return SecurityContext.builder()
-            .securityReferences(singletonList(securityReference))
-            .operationSelector {
-                it.requestMappingPattern().matches(Regex("^/(balanceOf|owner)/.*"))
-            }
-            .build()
-    }
-
-    @Bean
-    fun securityInfo(): SecurityConfiguration? {
-        return SecurityConfigurationBuilder.builder()
-            .clientId("admin-cli")
-            .realm("mixchange")
-            .appName("opex")
-            .scopeSeparator(",")
-            .build()
-    }
-}
+@EnableSwagger2
+class WalletApp
 
 fun main(args: Array<String>) {
     runApplication<WalletApp>(*args)
