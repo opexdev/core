5407bb1afa9316c02fb764d3f60ea9fb6111740e
maryarm <maryam@nilin.co>
2021-08-24 14:15:08 +0430

    Fix #37-Add Filter To fix FormParam problem in WebFlux

--- Api/api-ports/api-binance-rest/src/main/kotlin/co/nilin/opex/port/api/binance/config/RestConfig.kt
+++ Api/api-ports/api-binance-rest/src/main/kotlin/co/nilin/opex/port/api/binance/config/RestConfig.kt
@@ -1,9 +1,11 @@
 package co.nilin.opex.port.api.binance.config
 
+import co.nilin.opex.utility.interceptor.FormDataWorkaroundFilter
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.format.Formatter
 import java.util.*
+import org.springframework.web.server.WebFilter
 
 @Configuration
 class RestConfig {
@@ -18,5 +20,10 @@ class RestConfig {
                 return Date(date.toLong())
             }
         }
+    }
+
+    @Bean
+    fun formDataWebFilter(): WebFilter {
+        return FormDataWorkaroundFilter()
     }
 }
