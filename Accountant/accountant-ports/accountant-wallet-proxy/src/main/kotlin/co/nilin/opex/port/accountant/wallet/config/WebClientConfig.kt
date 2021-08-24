b62c1e5372c9073ff6e5403a2f30cc458c0b5558
maryarm <maryam@nilin.co>
2021-08-24 14:51:59 +0430

    Fix #38-Add logs for received Rest calls and WebClients

--- Accountant/accountant-ports/accountant-wallet-proxy/src/main/kotlin/co/nilin/opex/port/accountant/wallet/config/WebClientConfig.kt
+++ Accountant/accountant-ports/accountant-wallet-proxy/src/main/kotlin/co/nilin/opex/port/accountant/wallet/config/WebClientConfig.kt
@@ -1,19 +1,32 @@
 package co.nilin.opex.port.accountant.wallet.config
 
+import co.nilin.opex.utility.log.interceptor.CustomLogger
 import org.springframework.cloud.client.ServiceInstance
 import org.springframework.cloud.client.loadbalancer.LoadBalancerProperties
 import org.springframework.cloud.client.loadbalancer.reactive.ReactiveLoadBalancer
 import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.http.client.reactive.ReactorClientHttpConnector
 import org.springframework.web.reactive.function.client.WebClient
+import reactor.netty.http.client.HttpClient
 
 @Configuration
 class WebClientConfig {
 
     @Bean
     fun webClient(loadBalancerFactory: ReactiveLoadBalancer.Factory<ServiceInstance>): WebClient {
+        val logger =  CustomLogger(HttpClient::class.java)
         return WebClient.builder()
+            .clientConnector(
+                ReactorClientHttpConnector(
+                    HttpClient
+                        .create()
+                        .doOnRequest { request, connection ->
+                            connection.addHandlerFirst(logger)
+                        }
+                )
+            )
             .filter(
                 ReactorLoadBalancerExchangeFilterFunction(
                     loadBalancerFactory, LoadBalancerProperties(), emptyList()
---
+++ Utility/logging-handler/src/main/kotlin/co/nilin/opex/utility/log/interceptor/decorator/PayloadServerWebExchangeDecorator.java
@@ -0,0 +1,33 @@
+package co.nilin.opex.utility.log.interceptor.decorator;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.ServerWebExchangeDecorator;
+
+public class PayloadServerWebExchangeDecorator extends ServerWebExchangeDecorator {
+
+    private Logger log = LoggerFactory.getLogger( PayloadServerWebExchangeDecorator.class);
+
+    private PayloadBufferServerHttpRequestDecorator requestDecorator;
+
+    private PayloadBufferServerHttpResponseDecorator responseDecorator;
+
+    public PayloadServerWebExchangeDecorator(String tracing, ServerWebExchange delegate) {
+        super(delegate);
+        requestDecorator = new PayloadBufferServerHttpRequestDecorator(tracing, delegate.getRequest());
+        responseDecorator = new PayloadBufferServerHttpResponseDecorator(tracing, delegate.getResponse());
+    }
+
+    @Override
+    public ServerHttpRequest getRequest() {
+        return requestDecorator;
+    }
+
+    @Override
+    public ServerHttpResponse getResponse() {
+        return responseDecorator;
+    }
+}
