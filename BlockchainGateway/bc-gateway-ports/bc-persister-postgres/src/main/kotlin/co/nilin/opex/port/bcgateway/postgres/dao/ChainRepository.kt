060d8385c8bcf8eee0c0acdf865e54eff415c2a2
metalicn20 <metalicn20@gmail.com>
2021-09-05 15:59:57 +0430

    Update chain repository

--- BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/ChainRepository.kt
+++ BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/ChainRepository.kt
@@ -1,14 +1,11 @@
 package co.nilin.opex.port.bcgateway.postgres.dao
 
 import co.nilin.opex.port.bcgateway.postgres.model.ChainModel
-import co.nilin.opex.port.bcgateway.postgres.model.ChainModelProjection
 import kotlinx.coroutines.flow.Flow
-import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 
 @Repository
 interface ChainRepository : ReactiveCrudRepository<ChainModel, String> {
-    @Query("SELECT * FROM chain WHERE name = :name")
-    fun findByNameProjected(name: String): Flow<ChainModelProjection>
+    fun findByName(name: String): Flow<ChainModel>
 }
