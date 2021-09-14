83d50e927c9f7837aea0a5041f0d300322c72345
metalicn20 <metalicn20@gmail.com>
2021-09-14 14:36:41 +0430

    Implement wallet sync scheduler handler

--- BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/WalletSyncScheduleRepository.kt
+++ BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/WalletSyncScheduleRepository.kt
@@ -1,8 +1,14 @@
 package co.nilin.opex.port.bcgateway.postgres.dao
 
 import co.nilin.opex.port.bcgateway.postgres.model.WalletSyncScheduleModel
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 @Repository
-interface WalletSyncScheduleRepository : ReactiveCrudRepository<WalletSyncScheduleModel, Long>
+interface WalletSyncScheduleRepository : ReactiveCrudRepository<WalletSyncScheduleModel, Long> {
+    @Query("select * from wallet_sync_schedules where retryTime <= :time")
+    fun findActiveSchedule(time: LocalDateTime): Mono<WalletSyncScheduleModel>
+}
