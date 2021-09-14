c8c1b22ebb27c97803af8e53c0a0e016df44f235
metalicn20 <metalicn20@gmail.com>
2021-09-14 19:28:00 +0430

    Remove chain sync scheduler handler

--- BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/ChainSyncScheduleRepository.kt
+++ BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/ChainSyncScheduleRepository.kt
@@ -1,8 +1,16 @@
 package co.nilin.opex.port.bcgateway.postgres.dao
 
 import co.nilin.opex.port.bcgateway.postgres.model.ChainSyncScheduleModel
+import co.nilin.opex.port.bcgateway.postgres.model.WalletSyncScheduleModel
+import kotlinx.coroutines.flow.Flow
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
+import java.time.LocalDateTime
 
 @Repository
-interface ChainSyncScheduleRepository : ReactiveCrudRepository<ChainSyncScheduleModel, String>
+interface ChainSyncScheduleRepository : ReactiveCrudRepository<ChainSyncScheduleModel, String> {
+    @Query("select * from chain_sync_schedules where retry_time <= :time")
+    fun findActiveSchedule(time: LocalDateTime): Flow<ChainSyncScheduleModel>
+}
