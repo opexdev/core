ff15d0ae6fe0e7b6dd4ead6572c31cf7bbff9dff
metalicn20 <metalicn20@gmail.com>
2021-09-20 19:15:33 +0430

    Implement reserved addresses

--- BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/ReservedAddressRepository.kt
+++ BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/dao/ReservedAddressRepository.kt
@@ -1,8 +1,18 @@
 package co.nilin.opex.port.bcgateway.postgres.dao
 
 import co.nilin.opex.port.bcgateway.postgres.model.ReservedAddressModel
+import org.springframework.data.r2dbc.repository.Modifying
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
+import reactor.core.publisher.Mono
 
 @Repository
-interface ReservedAddressRepository : ReactiveCrudRepository<ReservedAddressModel, Long>
+interface ReservedAddressRepository : ReactiveCrudRepository<ReservedAddressModel, Long> {
+    @Query("select * from reserved_addresses where address_type = :addressType order by id DESC")
+    fun peekFirstAdded(addressType: Long): Mono<ReservedAddressModel>
+
+    @Modifying
+    @Query("delete from reserved_addresses where address = :address and (memo is null or memo = :memo)")
+    fun remove(address: String, memo: String?): Mono<Int>
+}
--- BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/impl/ReservedAddressHandlerImpl.kt
+++ BlockchainGateway/bc-gateway-ports/bc-persister-postgres/src/main/kotlin/co/nilin/opex/port/bcgateway/postgres/impl/ReservedAddressHandlerImpl.kt
@@ -3,15 +3,20 @@ package co.nilin.opex.port.bcgateway.postgres.impl
 import co.nilin.opex.bcgateway.core.model.AddressType
 import co.nilin.opex.bcgateway.core.model.ReservedAddress
 import co.nilin.opex.bcgateway.core.spi.ReservedAddressHandler
+import co.nilin.opex.port.bcgateway.postgres.dao.ReservedAddressRepository
+import kotlinx.coroutines.reactive.awaitFirst
+import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.springframework.stereotype.Component
 
 @Component
-class ReservedAddressHandlerImpl: ReservedAddressHandler {
+class ReservedAddressHandlerImpl(private val reservedAddressRepository: ReservedAddressRepository) :
+    ReservedAddressHandler {
     override suspend fun peekReservedAddress(addressType: AddressType): ReservedAddress? {
-        TODO("Not yet implemented")
+        return reservedAddressRepository.peekFirstAdded(addressType.id)
+            .map { ReservedAddress(it.address, it.memo, addressType) }.awaitFirstOrNull()
     }
 
     override suspend fun remove(reservedAddress: ReservedAddress) {
-        TODO("Not yet implemented")
+        reservedAddressRepository.remove(reservedAddress.address, reservedAddress.memo).awaitFirst()
     }
 }
