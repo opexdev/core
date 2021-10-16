3680071db6d48ad64c6dc7da92791c25293f0357
metalicn20 <metalicn20@gmail.com>
2021-10-16 13:07:50 +0330

    Add storage module and directory

---
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/app/StorageApp.kt
@@ -0,0 +1,15 @@
+package co.nilin.opex.app
+
+import co.nilin.opex.utility.error.EnableOpexErrorHandler
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.ComponentScan
+
+@SpringBootApplication
+@ComponentScan("co.nilin.opex")
+@EnableOpexErrorHandler
+class AccountantApp
+
+fun main(args: Array<String>) {
+    runApplication<AccountantApp>(*args)
+}
---
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/app/controller/FileController.kt
@@ -0,0 +1,6 @@
+package co.nilin.opex.app.controller
+
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class FileController
