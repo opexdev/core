fa45828d2ecbedb7b1a3c4cc5e9b5b9a11a2d10d
metalicn20 <metalicn20@gmail.com>
2021-10-17 16:14:46 +0330

    Add get file controller

--- Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/controller/FileController.kt
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/controller/FileController.kt
@@ -1,18 +1,35 @@
 package co.nilin.opex.storage.app.controller
 
 import co.nilin.opex.storage.app.service.StorageService
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.http.HttpHeaders
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 import org.springframework.web.multipart.MultipartFile
+import org.springframework.core.io.Resource
 
 @RestController
 class FileController(private val storageService: StorageService) {
     @PostMapping("/{uid}")
-    suspend fun fileUpload(@PathVariable("uid") uid: String, @RequestParam("file") file: MultipartFile) {
+    suspend fun fileUpload(
+        @PathVariable("uid") uid: String,
+        @RequestParam("file") file: MultipartFile
+    ): ResponseEntity<String> {
         val ext = file.name.replace(Regex(".+(?=\\..+)"), "")
-        if (ext !in listOf(".jpg", ".png", ".mp4", ".mov")) throw Exception("Invalid File Format")
+        if (ext !in listOf(".jpg", ".png", ".mp4", ".mov")) return ResponseEntity.badRequest()
+            .body("Invalid File Format")
         storageService.store("/opex-storage/$uid/${file.name}", file)
+        return ResponseEntity.ok().build()
+    }
+
+    @GetMapping("/{uid}/{filename}")
+    suspend fun fileDownload(
+        @PathVariable("uid") uid: String,
+        @PathVariable("filename") filename: String
+    ): ResponseEntity<Resource> {
+        val resource = storageService.loadAsResource("/opex-storage/$uid/$filename")
+        return ResponseEntity.ok().header(
+            HttpHeaders.CONTENT_DISPOSITION,
+            "attachment; filename=\"" + resource.filename + "\""
+        ).body(resource);
     }
 }
