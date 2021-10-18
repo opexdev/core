a69eed67d293b30ec8978f86ac45d3cd8865d681
metalicn20 <metalicn20@gmail.com>
2021-10-18 16:10:46 +0330

    Implement storage authentication

--- Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/controller/FileController.kt
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/controller/FileController.kt
@@ -5,6 +5,8 @@ import kotlinx.coroutines.reactive.awaitFirst
 import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.http.codec.multipart.FilePart
+import org.springframework.security.core.annotation.CurrentSecurityContext
+import org.springframework.security.core.context.SecurityContext
 import org.springframework.web.bind.annotation.*
 import reactor.core.publisher.Mono
 import java.net.URLConnection
@@ -15,15 +17,17 @@ class FileController(private val storageService: StorageService) {
     @PostMapping("/{uid}")
     suspend fun fileUpload(
         @PathVariable("uid") uid: String,
-        @RequestPart("file") file: Mono<FilePart>
+        @RequestPart("file") file: Mono<FilePart>,
+        @CurrentSecurityContext securityContext: SecurityContext
     ): ResponseEntity<String> {
+        if (securityContext.authentication.name !== uid) return ResponseEntity.status(401).build()
         file.awaitFirst().apply {
             val ext = this.filename().replace(Regex(".+(?=\\..+)"), "")
             if (ext !in listOf(".jpg", ".png", ".mp4", ".mov")) return ResponseEntity.badRequest()
                 .body("Invalid File Format")
             val path = Paths.get("").resolve("/opex-storage/$uid/${this.filename()}").toString()
             storageService.store(path, this)
-            return ResponseEntity.ok().body(path)
+            return ResponseEntity.ok().build()
         }
     }
 
@@ -31,8 +35,10 @@ class FileController(private val storageService: StorageService) {
     @ResponseBody
     suspend fun fileDownload(
         @PathVariable("uid") uid: String,
-        @PathVariable("filename") filename: String
+        @PathVariable("filename") filename: String,
+        @CurrentSecurityContext securityContext: SecurityContext
     ): ResponseEntity<ByteArray> {
+        if (securityContext.authentication.name !== uid) return ResponseEntity.status(401).build()
         val path = Paths.get("").resolve("/opex-storage/$uid/$filename")
         val file = storageService.load(path.toString())
         val mimeType = URLConnection.getFileNameMap().getContentTypeFor(path.fileName.toString())
