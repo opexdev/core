b0deda2af80b50a6aa77d191fb24aa3e1431b8b9
Ebrahim Hosseiny Fadaee <metalicn20@gmail.com>
2021-10-19 11:35:55 +0330

    Close #108, Implement upload/download file (#109)
    * Add storage module and directory
    
    * Rename package names
    
    * Implement controller
    
    * Fix poms
    
    * Add storage service
    
    * Add get file controller
    
    * Add storage module to docker-compose.yml
    
    * Implement get file
    
    * Implement storage authentication
    
    * Fix storage authentication
    
    * Fix storage save method
    
    * Cleanup imports

---
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/config/AppConfig.kt
@@ -0,0 +1,6 @@
+package co.nilin.opex.storage.app.config
+
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+class AppConfig
---
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/controller/FileController.kt
@@ -0,0 +1,48 @@
+package co.nilin.opex.storage.app.controller
+
+import co.nilin.opex.storage.app.service.StorageService
+import kotlinx.coroutines.reactive.awaitFirstOrNull
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
+import org.springframework.http.codec.multipart.FilePart
+import org.springframework.security.core.annotation.CurrentSecurityContext
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.web.bind.annotation.*
+import reactor.core.publisher.Mono
+import java.net.URLConnection
+import java.nio.file.Paths
+
+@RestController
+class FileController(private val storageService: StorageService) {
+    @PostMapping("/{uid}")
+    suspend fun fileUpload(
+        @PathVariable("uid") uid: String,
+        @RequestPart("file") file: Mono<FilePart>,
+        @CurrentSecurityContext securityContext: SecurityContext
+    ): ResponseEntity<String> {
+        if (securityContext.authentication.name != uid) return ResponseEntity.status(401).build()
+        file.awaitFirstOrNull().apply {
+            if (this == null) return ResponseEntity.badRequest().build()
+            val ext = this.filename().replace(Regex(".+(?=\\..+)"), "")
+            if (ext !in listOf(".jpg", ".png", ".mp4", ".mov")) return ResponseEntity.badRequest()
+                .body("Invalid File Format")
+            val path = Paths.get("").resolve("/opex-storage/$uid/${this.filename()}").toString()
+            storageService.store(path, this)
+            return ResponseEntity.ok().build()
+        }
+    }
+
+    @GetMapping("/{uid}/{filename}")
+    @ResponseBody
+    suspend fun fileDownload(
+        @PathVariable("uid") uid: String,
+        @PathVariable("filename") filename: String,
+        @CurrentSecurityContext securityContext: SecurityContext
+    ): ResponseEntity<ByteArray> {
+        if (securityContext.authentication.name != uid) return ResponseEntity.status(401).build()
+        val path = Paths.get("").resolve("/opex-storage/$uid/$filename")
+        val file = storageService.load(path.toString())
+        val mimeType = URLConnection.getFileNameMap().getContentTypeFor(path.fileName.toString())
+        return ResponseEntity.ok().contentType(MediaType.parseMediaType(mimeType)).body(file.readBytes())
+    }
+}
