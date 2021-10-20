796c50c18876cf1fda371dd3ccbdec2f6640fce7
metalicn20 <metalicn20@gmail.com>
2021-10-20 12:46:12 +0330

    Add cors to storage

---
+++ Storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/config/CorsConfig.kt
@@ -0,0 +1,25 @@
+package co.nilin.opex.storage.app.config
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Configuration
+import org.springframework.web.reactive.config.CorsRegistry
+import org.springframework.web.reactive.config.WebFluxConfigurer
+
+@Configuration
+class CorsConfig : WebFluxConfigurer {
+
+    @Value("\${app.cors.allowed-hosts}")
+    private lateinit var hosts: Array<String>
+
+    @Value("\${app.cors.allowed-patterns}")
+    private lateinit var patterns: Array<String>
+
+    override fun addCorsMappings(registry: CorsRegistry) {
+        registry.addMapping("/**")
+            .allowedOrigins(*hosts)
+            .allowedOriginPatterns(*patterns)
+            .allowedHeaders("*")
+            .allowedMethods("*")
+    }
+
+}
