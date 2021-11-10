7ca01209fc642bf2b5f81011f34492fa44997dbe
metalicn20 <metalicn20@gmail.com>
2021-11-10 12:02:10 +0330

    Improve accountant data.sql

--- Accountant/accountant-ports/accountant-persister-postgres/src/main/resources/data.sql
+++ Accountant/accountant-ports/accountant-persister-postgres/src/main/resources/data.sql
@@ -1,89 +1,21 @@
 INSERT INTO
-   PAIR_CONFIG
+   pair_config
 VALUES
-   (
-      'btc_usdt',
-      'btc',
-      'usdt',
-      0.000001,
-      0.01,
-      55000
-   ) ON CONFLICT DO NOTHING;
+   ('btc_usdt', 'btc', 'usdt', 0.000001, 0.01, 55000),
+   ('eth_usdt', 'eth', 'usdt', 0.00001, 0.01, 3800),
+   ('nln_usdt', 'nln', 'usdt', 1.0, 0.01, 0.01),
+   ('nln_btc', 'nln', 'btc', 1.0, 0.000001, 1 / 5500000) ON CONFLICT DO NOTHING;
 
 INSERT INTO
-   PAIR_FEE_CONFIG
+   pair_fee_config
 VALUES
-   (1, 'btc_usdt', 'ASK', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
-   (2, 'btc_usdt', 'BID', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_CONFIG
-VALUES
-   (
-      'eth_usdt',
-      'eth',
-      'usdt',
-      0.00001,
-      0.01,
-      3800
-   ) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
-   (7, 'eth_usdt', 'ASK', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
+   (1, 'btc_usdt', 'ASK', '*', 0.01, 0.01),
+   (2, 'btc_usdt', 'BID', '*', 0.01, 0.01),
+   (3, 'nln_usdt', 'ASK', '*', 0.01, 0.01),
+   (4, 'nln_usdt', 'BID', '*', 0.01, 0.01),
+   (5, 'nln_btc', 'ASK', '*', 0.01, 0.01),
+   (6, 'nln_btc', 'BID', '*', 0.01, 0.01),
+   (7, 'eth_usdt', 'ASK', '*', 0.01, 0.01),
    (8, 'eth_usdt', 'BID', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_CONFIG
-VALUES
-   (
-      'nln_usdt',
-      'nln',
-      'usdt',
-      1.0,
-      0.01,
-      0.01
-   ) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
-   (3, 'nln_usdt', 'ASK', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
-   (4, 'nln_usdt', 'BID', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_CONFIG
-VALUES
-   (
-      'nln_btc',
-      'nln',
-      'btc',
-      1.0,
-      0.000001,
-      1 / 5500000
-   ) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
-   (5, 'nln_btc', 'ASK', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
-
-INSERT INTO
-   PAIR_FEE_CONFIG
-VALUES
-   (6, 'nln_btc', 'BID', '*', 0.01, 0.01) ON CONFLICT DO NOTHING;
 
 COMMIT;
