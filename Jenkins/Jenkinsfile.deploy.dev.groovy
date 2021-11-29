0e34841dd65171238cd7839eaa8f985bc293eeb4
metalicn20 <metalicn20@gmail.com>
2021-11-29 11:14:02 +0330

    Separate deploy configs

--- Jenkins/Jenkinsfile.deploy.dev.groovy
+++ Jenkins/Jenkinsfile.deploy.dev.groovy
@@ -1,7 +1,7 @@
 pipeline {
     agent any
 
-    stages('Deploy - Production') {
+    stages('Deploy - Stage') {
         stage('Build') {
             steps {
                 setBuildStatus("?", "PENDING")
@@ -14,11 +14,11 @@ pipeline {
         }
         stage('Deliver') {
             environment {
-                DATA = '/var/opex/runtime'
+                DATA = '/var/opex/runtime-stage'
             }
             steps {
                 dir("Deployment") {
-                    sh 'docker-compose up -d --build'
+                    sh 'docker-compose -f docker-compose.yml -f docker-compose.dev.yml  up -d --build'
                 }
             }
         }
