934a882360452e58fc07999f1c64f450eb85d451
Peyman <marchosiax@gmail.com>
2021-11-29 17:40:44 +0330

    clean up trade manager

--- accountant/accountant-core/src/main/kotlin/co/nilin/opex/accountant/core/service/TradeManagerImpl.kt
+++ accountant/accountant-core/src/main/kotlin/co/nilin/opex/accountant/core/service/TradeManagerImpl.kt
@@ -3,6 +3,7 @@ package co.nilin.opex.accountant.core.service
 import co.nilin.opex.accountant.core.api.TradeManager
 import co.nilin.opex.accountant.core.inout.RichTrade
 import co.nilin.opex.accountant.core.model.FinancialAction
+import co.nilin.opex.accountant.core.model.Order
 import co.nilin.opex.accountant.core.spi.*
 import co.nilin.opex.matching.engine.core.eventh.events.TradeEvent
 import co.nilin.opex.matching.engine.core.model.OrderDirection
@@ -12,15 +13,15 @@ import java.math.BigDecimal
 import java.time.LocalDateTime
 
 open class TradeManagerImpl(
-    val pairStaticRateLoader: PairStaticRateLoader,
-    val financeActionPersister: FinancialActionPersister,
-    val financeActionLoader: FinancialActionLoader,
-    val orderPersister: OrderPersister,
-    val tempEventPersister: TempEventPersister,
-    val richTradePublisher: RichTradePublisher,
-    val walletProxy: WalletProxy,
-    val platformCoin: String,
-    val platformAddress: String
+    private val pairStaticRateLoader: PairStaticRateLoader,
+    private val financeActionPersister: FinancialActionPersister,
+    private val financeActionLoader: FinancialActionLoader,
+    private val orderPersister: OrderPersister,
+    private val tempEventPersister: TempEventPersister,
+    private val richTradePublisher: RichTradePublisher,
+    private val walletProxy: WalletProxy,
+    private val platformCoin: String,
+    private val platformAddress: String
 ) : TradeManager {
 
     private val log = LoggerFactory.getLogger(TradeManagerImpl::class.java)
@@ -33,6 +34,7 @@ open class TradeManagerImpl(
         val takerOrder = orderPersister.load(trade.takerOuid)
         //maker order by ouid
         val makerOrder = orderPersister.load(trade.makerOuid)
+
         if (takerOrder == null || makerOrder == null) {
             if (takerOrder == null) {
                 tempEventPersister.saveTempEvent(trade.takerOuid, trade)
@@ -42,38 +44,16 @@ open class TradeManagerImpl(
             }
             return emptyList()
         }
+
         //check taker uuid
-        //
         //check maker uuid
-        //
-        val leftSidePCRate = pairStaticRateLoader.calculateStaticRate(platformCoin, trade.pair.leftSideName)
-        val rightSidePCRate = pairStaticRateLoader.calculateStaticRate(platformCoin, trade.pair.rightSideName)
 
-        val takerMatchedAmount = if (takerOrder.direction == OrderDirection.ASK) {
-            trade.matchedQuantity.toBigDecimal().multiply(takerOrder.leftSideFraction.toBigDecimal())
-        } else {
-            trade.matchedQuantity.toBigDecimal().multiply(takerOrder.leftSideFraction.toBigDecimal())
-                .multiply(trade.makerPrice.toBigDecimal()).multiply(takerOrder.rightSideFraction.toBigDecimal())
-        }
+        val takerMatchedAmount = calculateMatchedAmount(trade, takerOrder)
+        val takerPCFeeCoefficient = getPCFeeCoefficient(trade, takerOrder)
 
-        val takerPCFeeCoefficient: Double = if (takerOrder.direction == OrderDirection.ASK) {
-            leftSidePCRate ?: 0.0
-        } else {
-            rightSidePCRate ?: 0.0
-        }
+        val makerMatchedAmount = calculateMatchedAmount(trade, makerOrder)
+        val makerPCFeeCoefficient = getPCFeeCoefficient(trade, makerOrder)
 
-        val makerMatchedAmount = if (makerOrder.direction == OrderDirection.ASK) {
-            trade.matchedQuantity.toBigDecimal().multiply(makerOrder.leftSideFraction.toBigDecimal())
-        } else {
-            trade.matchedQuantity.toBigDecimal().multiply(makerOrder.leftSideFraction.toBigDecimal())
-                .multiply(trade.makerPrice.toBigDecimal()).multiply(makerOrder.rightSideFraction.toBigDecimal())
-        }
-
-        val makerPCFeeCoefficient: Double = if (makerOrder.direction == OrderDirection.ASK) {
-            leftSidePCRate ?: 0.0
-        } else {
-            rightSidePCRate ?: 0.0
-        }
         log.info("trade event configs loaded ")
         //lookup for taker parent fa
         val takerParentFinancialAction = financeActionLoader.findLast(trade.takerUuid, trade.takerOuid)
@@ -85,68 +65,30 @@ open class TradeManagerImpl(
 
         //calculate maker fee
         val makerFee = makerOrder.makerFee
-        val makerTotalFeeWithPlatformCoin = takerMatchedAmount
-            .multiply(makerFee.toBigDecimal())
-            .multiply(makerPCFeeCoefficient.toBigDecimal())
+        val makerTotalFeeWithPlatformCoin = calcFeeWithPlatformCoin(makerMatchedAmount, makerFee, makerPCFeeCoefficient)
+
         //check if maker uuid can pay the fee with platform coin
         //create fa for transfer taker uuid symbol exchange wallet to maker symbol main wallet
         /*
-        amount for sell (ask): match_quantity (if not pay by platform coin then - maker fee)
-        amount for buy (bid): match_quantity * maker price (if not pay by platform coin then - maker fee)
-         */
-        val takerTransferAction = FinancialAction(
-            takerParentFinancialAction,
-            TradeEvent::class.simpleName!!,
-            trade.takerOuid,
-            if (takerOrder.direction == OrderDirection.ASK) {
-                trade.pair.leftSideName
-            } else {
-                trade.pair.rightSideName
-            },
-            takerMatchedAmount,
-            trade.takerUuid,
-            "exchange",
-            trade.makerUuid,
-            "main",
-            LocalDateTime.now()
-        )
+        * amount for sell (ask): match_quantity (if not pay by platform coin then - maker fee)
+        * amount for buy (bid): match_quantity * maker price (if not pay by platform coin then - maker fee)
+        */
+        val takerTransferAction =
+            createTransferAction(trade, takerParentFinancialAction, takerOrder, takerMatchedAmount)
         log.info("trade event takerTransferAction {}", takerTransferAction)
         financialActions.add(takerTransferAction)
-        val makerFeeAction = if (makerTotalFeeWithPlatformCoin > BigDecimal.ZERO &&
-            walletProxy.canFulfil(platformCoin, "main", trade.makerUuid, makerTotalFeeWithPlatformCoin)
-        ) {
-            FinancialAction(
-                makerParentFinancialAction,
-                TradeEvent::class.simpleName!!,
-                trade.takerOuid,
-                platformCoin,
-                makerTotalFeeWithPlatformCoin,
-                trade.makerUuid,
-                "main",
-                platformAddress,
-                "exchange",
-                LocalDateTime.now()
-            )
-        } else {
-            FinancialAction(
-                makerParentFinancialAction,
-                TradeEvent::class.simpleName!!,
-                trade.takerOuid,
-                if (takerOrder.direction == OrderDirection.ASK) {
-                    trade.pair.leftSideName
-                } else {
-                    trade.pair.rightSideName
-                },
-                takerMatchedAmount.multiply(makerFee.toBigDecimal()),
-                trade.makerUuid,
-                "main",
-                platformAddress,
-                "exchange",
-                LocalDateTime.now()
-            )
-        }
+
+        val makerFeeAction = createMakerFeeAction(
+            trade,
+            makerParentFinancialAction,
+            makerTotalFeeWithPlatformCoin,
+            makerFee,
+            takerOrder,
+            takerMatchedAmount
+        )
         log.info("trade event makerFeeAction {}", makerFeeAction)
         financialActions.add(makerFeeAction)
+
         //update taker order status
         takerOrder.remainedTransferAmount -= takerMatchedAmount
         if (takerOrder.filledQuantity == takerOrder.quantity) {
@@ -157,72 +99,28 @@ open class TradeManagerImpl(
 
         //calculate taker fee
         val takerFee = takerOrder.takerFee
-        val takerTotalFeeWithPlatformCoin = takerMatchedAmount
-            .multiply(takerFee.toBigDecimal())
-            .multiply(takerPCFeeCoefficient.toBigDecimal())
+        val takerTotalFeeWithPlatformCoin = calcFeeWithPlatformCoin(takerMatchedAmount, takerFee, takerPCFeeCoefficient)
 
         //create fa for transfer makeruuid symbol exchange wallet to taker symbol main wallet
         /*
         amount for sell (ask): match_quantity (if not pay by platform coin then - taker fee)
         amount for buy (bid): match_quantity * maker price (if not pay by platform coin then - taker fee)
          */
-        val makerTransferAction = FinancialAction(
-            makerParentFinancialAction,
-            TradeEvent::class.simpleName!!,
-            trade.makerOuid,
-            if (makerOrder.direction == OrderDirection.ASK) {
-                trade.pair.leftSideName
-            } else {
-                trade.pair.rightSideName
-            },
-            makerMatchedAmount,
-            trade.makerUuid,
-            "exchange",
-            trade.takerUuid,
-            "main",
-            LocalDateTime.now()
-        )
+        val makerTransferAction =
+            createTransferAction(trade, makerParentFinancialAction, makerOrder, makerMatchedAmount)
         log.info("trade event makerTransferAction {}", makerTransferAction)
         financialActions.add(makerTransferAction)
-        //check if taker uuid can pay the fee with platform coin
-        val takerFeeAction = if (takerTotalFeeWithPlatformCoin > BigDecimal.ZERO &&
-            walletProxy.canFulfil(platformCoin, "main", trade.takerUuid, takerTotalFeeWithPlatformCoin)
-        ) {
-            FinancialAction(
-                takerParentFinancialAction,
-                TradeEvent::class.simpleName!!,
-                trade.makerOuid,
-                if (makerOrder.direction == OrderDirection.ASK) {
-                    trade.pair.leftSideName
-                } else {
-                    trade.pair.rightSideName
-                },
-                takerTotalFeeWithPlatformCoin,
-                trade.takerUuid,
-                "main",
-                platformAddress,
-                "",
-                LocalDateTime.now()
-            )
-        } else {
-            FinancialAction(
-                takerParentFinancialAction,
-                TradeEvent::class.simpleName!!,
-                trade.makerOuid,
-                if (makerOrder.direction == OrderDirection.ASK) {
-                    trade.pair.leftSideName
-                } else {
-                    trade.pair.rightSideName
-                },
-                makerMatchedAmount.multiply(takerFee.toBigDecimal()),
-                trade.takerUuid,
-                "main",
-                platformAddress,
-                "exchange",
-                LocalDateTime.now()
-            )
 
-        }
+        //check if taker uuid can pay the fee with platform coin
+        val takerFeeAction = createTakerFeeAction(
+            trade,
+            takerParentFinancialAction,
+            takerTotalFeeWithPlatformCoin,
+            takerFee,
+            makerOrder,
+            makerMatchedAmount
+        )
+
         log.info("trade event takerFeeAction {}", takerFeeAction)
         financialActions.add(takerFeeAction)
         //update maker order status
@@ -262,5 +160,144 @@ open class TradeManagerImpl(
 
         )
         return financeActionPersister.persist(financialActions)
+    }
+
+    private fun calculateMatchedAmount(trade: TradeEvent, order: Order): BigDecimal {
+        return if (order.direction == OrderDirection.ASK) {
+            trade.matchedQuantity.toBigDecimal().multiply(order.leftSideFraction.toBigDecimal())
+        } else {
+            trade.matchedQuantity.toBigDecimal().multiply(order.leftSideFraction.toBigDecimal())
+                .multiply(trade.makerPrice.toBigDecimal()).multiply(order.rightSideFraction.toBigDecimal())
+        }
+    }
+
+    private suspend fun getPCFeeCoefficient(trade: TradeEvent, order: Order): Double {
+        val leftSidePCRate = pairStaticRateLoader.calculateStaticRate(platformCoin, trade.pair.leftSideName)
+        val rightSidePCRate = pairStaticRateLoader.calculateStaticRate(platformCoin, trade.pair.rightSideName)
+        return if (order.direction == OrderDirection.ASK) {
+            leftSidePCRate ?: 0.0
+        } else {
+            rightSidePCRate ?: 0.0
+        }
+    }
+
+    private fun calcFeeWithPlatformCoin(
+        matchedAmount: BigDecimal,
+        fee: Double,
+        coefficient: Double
+    ): BigDecimal {
+        return matchedAmount.multiply(fee.toBigDecimal()).multiply(coefficient.toBigDecimal())
+    }
+
+    private fun createTransferAction(
+        trade: TradeEvent,
+        parentAction: FinancialAction?,
+        order: Order,
+        matchedAmount: BigDecimal
+    ): FinancialAction {
+        return FinancialAction(
+            parentAction,
+            TradeEvent::class.simpleName!!,
+            trade.takerOuid,
+            if (order.direction == OrderDirection.ASK) {
+                trade.pair.leftSideName
+            } else {
+                trade.pair.rightSideName
+            },
+            matchedAmount,
+            trade.takerUuid,
+            "exchange",
+            trade.makerUuid,
+            "main",
+            LocalDateTime.now()
+        )
+    }
+
+    private suspend fun createMakerFeeAction(
+        trade: TradeEvent,
+        parentAction: FinancialAction?,
+        totalFee: BigDecimal,
+        makerFee: Double,
+        takerOrder: Order,
+        takerMatchedAmount: BigDecimal,
+    ): FinancialAction {
+        val canFulfill = walletProxy.canFulfil(platformCoin, "main", trade.makerUuid, totalFee)
+        return if (totalFee > BigDecimal.ZERO && canFulfill) {
+            FinancialAction(
+                parentAction,
+                TradeEvent::class.simpleName!!,
+                trade.takerOuid,
+                platformCoin,
+                totalFee,
+                trade.makerUuid,
+                "main",
+                platformAddress,
+                "exchange",
+                LocalDateTime.now()
+            )
+        } else {
+            FinancialAction(
+                parentAction,
+                TradeEvent::class.simpleName!!,
+                trade.takerOuid,
+                if (takerOrder.direction == OrderDirection.ASK) {
+                    trade.pair.leftSideName
+                } else {
+                    trade.pair.rightSideName
+                },
+                takerMatchedAmount.multiply(makerFee.toBigDecimal()),
+                trade.makerUuid,
+                "main",
+                platformAddress,
+                "exchange",
+                LocalDateTime.now()
+            )
+        }
+    }
+
+    private suspend fun createTakerFeeAction(
+        trade: TradeEvent,
+        parentAction: FinancialAction?,
+        totalFee: BigDecimal,
+        takerFee: Double,
+        makerOrder: Order,
+        makerMatchedAmount: BigDecimal,
+    ): FinancialAction {
+        val canFulfill = walletProxy.canFulfil(platformCoin, "main", trade.takerUuid, totalFee)
+        return if (totalFee > BigDecimal.ZERO && canFulfill) {
+            FinancialAction(
+                parentAction,
+                TradeEvent::class.simpleName!!,
+                trade.makerOuid,
+                if (makerOrder.direction == OrderDirection.ASK) {
+                    trade.pair.leftSideName
+                } else {
+                    trade.pair.rightSideName
+                },
+                totalFee,
+                trade.takerUuid,
+                "main",
+                platformAddress,
+                "",
+                LocalDateTime.now()
+            )
+        } else {
+            FinancialAction(
+                parentAction,
+                TradeEvent::class.simpleName!!,
+                trade.makerOuid,
+                if (makerOrder.direction == OrderDirection.ASK) {
+                    trade.pair.leftSideName
+                } else {
+                    trade.pair.rightSideName
+                },
+                makerMatchedAmount.multiply(takerFee.toBigDecimal()),
+                trade.takerUuid,
+                "main",
+                platformAddress,
+                "exchange",
+                LocalDateTime.now()
+            )
+        }
     }
 }
