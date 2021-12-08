6ebb0c3330a65ef93837d99eed69d2e44caf12a1
metalicn20 <metalicn20@gmail.com>
2021-12-08 13:48:28 +0330

    Update ReferralHandler.kt

--- referral/referral-core/src/main/kotlin/co/nilin/opex/referral/core/spi/ReferralHandler.kt
+++ referral/referral-core/src/main/kotlin/co/nilin/opex/referral/core/spi/ReferralHandler.kt
@@ -3,5 +3,5 @@ package co.nilin.opex.referral.core.spi
 import co.nilin.opex.referral.core.model.Referral
 
 interface ReferralHandler {
-    fun findReferralByUuid(uuid: String): Referral
+    fun findReferralByUuid(uuid: String): Referral?
 }
