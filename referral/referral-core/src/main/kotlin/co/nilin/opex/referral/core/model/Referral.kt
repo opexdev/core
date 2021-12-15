b4360bb35e51cd03f75066819e4f6b503c342b39
metalicn20 <metalicn20@gmail.com>
2021-12-15 12:33:32 +0330

    Update referral object

--- referral/referral-core/src/main/kotlin/co/nilin/opex/referral/core/model/Referral.kt
+++ referral/referral-core/src/main/kotlin/co/nilin/opex/referral/core/model/Referral.kt
@@ -1,11 +1,6 @@
 package co.nilin.opex.referral.core.model
 
-import java.math.BigDecimal
-
 data class Referral(
-    var code: String,
-    var referrerUuid: String,
+    var referralCode: ReferralCode,
     var referentUuid: String,
-    var referrerCommission: BigDecimal,
-    var referentCommission: BigDecimal
 )
