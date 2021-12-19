d8f3409de746f5873fdfd6545fe19e9ff2781416
metalicn20 <metalicn20@gmail.com>
2021-12-19 16:10:13 +0330

    Implement commission persister

--- referral/referral-ports/referral-persister-postgres/src/main/kotlin/co/nilin/opex/referral/ports/postgres/impl/CommissionRewardPersisterImpl.kt
+++ referral/referral-ports/referral-persister-postgres/src/main/kotlin/co/nilin/opex/referral/ports/postgres/impl/CommissionRewardPersisterImpl.kt
@@ -2,11 +2,24 @@ package co.nilin.opex.referral.ports.postgres.impl
 
 import co.nilin.opex.referral.core.model.CommissionReward
 import co.nilin.opex.referral.core.spi.CommissionRewardPersister
+import co.nilin.opex.referral.ports.postgres.repository.CommissionRewardRepository
+import kotlinx.coroutines.reactive.awaitSingleOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class CommissionRewardPersisterImpl : CommissionRewardPersister {
+class CommissionRewardPersisterImpl(private val commissionRewardRepository: CommissionRewardRepository) :
+    CommissionRewardPersister {
     override suspend fun save(commissionReward: CommissionReward) {
-        println(commissionReward)
+        commissionRewardRepository.save(
+            co.nilin.opex.referral.ports.postgres.dao.CommissionReward(
+                null,
+                commissionReward.referralCode,
+                commissionReward.referrerUuid,
+                commissionReward.referentUuid,
+                commissionReward.richTrade.first,
+                commissionReward.referrerShare,
+                commissionReward.referentShare
+            )
+        ).awaitSingleOrNull()
     }
 }
