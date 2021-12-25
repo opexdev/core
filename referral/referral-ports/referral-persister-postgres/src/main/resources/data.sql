f00d9c4bea6d73187cfdea8b1ad0e335b3fcd1c9
metalicn20 <metalicn20@gmail.com>
2021-12-25 12:14:52 +0330

    Add payment_status initial data

--- referral/referral-ports/referral-persister-postgres/src/main/resources/data.sql
+++ referral/referral-ports/referral-persister-postgres/src/main/resources/data.sql
@@ -1,4 +1,3 @@
-INSERT INTO
-   configs(name, referral_commission_reward)
-VALUES
-   ('default', 0.3) ON CONFLICT DO NOTHING;
+INSERT INTO configs(name, referral_commission_reward) VALUES ('default', 0.3) ON CONFLICT DO NOTHING;
+
+INSERT INTO payment_status(status) VALUES ('pending'), ('checked_out') ON CONFLICT DO NOTHING;
