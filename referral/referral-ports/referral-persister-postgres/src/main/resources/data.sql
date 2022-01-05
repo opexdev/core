4c74960e950bd099127d41dd50db69f6c9ca3f6c
metalicn20 <metalicn20@gmail.com>
2022-01-05 15:08:39 +0330

    Fix data init script

--- referral/referral-ports/referral-persister-postgres/src/main/resources/data.sql
+++ referral/referral-ports/referral-persister-postgres/src/main/resources/data.sql
@@ -1,3 +1,3 @@
-INSERT INTO configs(name, referral_commission_reward, payment_asset_symbol, min_payment_amount) VALUES ('default', 0.3, 'usdt', 0) ON CONFLICT DO NOTHING;
+INSERT INTO configs(name, referral_commission_reward, payment_currency, min_payment_amount) VALUES ('default', 0.3, 'usdt', 0) ON CONFLICT DO NOTHING;
 
-INSERT INTO checkout_states(status) VALUES ('PENDING'), ('CHECKED_OUT') ON CONFLICT DO NOTHING;
+INSERT INTO checkout_states(state) VALUES ('PENDING'), ('CHECKED_OUT') ON CONFLICT DO NOTHING;
