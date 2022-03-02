000865a9da5aff6532f896c23ad07fb73f32b3d8
metalicn20 <metalicn20@gmail.com>
2022-03-02 11:14:36 +0330

    storage: Fix swagger

--- storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/config/SecurityConfig.kt
+++ storage/storage-app/src/main/kotlin/co/nilin/opex/storage/app/config/SecurityConfig.kt
@@ -24,6 +24,7 @@ class SecurityConfig(private val webClient: WebClient) {
             .pathMatchers("/actuator/**").permitAll()
             .pathMatchers("/swagger-ui/**").permitAll()
             .pathMatchers("/swagger-resources/**").permitAll()
+            .pathMatchers("/v2/api-docs").permitAll()
             .pathMatchers("/admin/**").hasRole("SCOPE_trust", "finance-admin")
             .pathMatchers("/**").hasAuthority("SCOPE_trust")
             .anyExchange().authenticated()
