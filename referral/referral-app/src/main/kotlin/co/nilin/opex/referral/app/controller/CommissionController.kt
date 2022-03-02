9798715b85cf6032d7ca192cc0102acaf2b7c7ee
metalicn20 <metalicn20@gmail.com>
2022-03-02 12:59:03 +0330

    referral: Add api doc to commission controller

--- referral/referral-app/src/main/kotlin/co/nilin/opex/referral/app/controller/CommissionController.kt
+++ referral/referral-app/src/main/kotlin/co/nilin/opex/referral/app/controller/CommissionController.kt
@@ -2,6 +2,10 @@ package co.nilin.opex.referral.app.controller
 
 import co.nilin.opex.matching.engine.core.model.OrderDirection
 import co.nilin.opex.referral.core.spi.CommissionRewardHandler
+import io.swagger.annotations.ApiOperation
+import io.swagger.annotations.ApiResponse
+import io.swagger.annotations.Example
+import io.swagger.annotations.ExampleProperty
 import org.springframework.web.bind.annotation.*
 import java.math.BigDecimal
 import java.time.ZoneId
@@ -19,6 +23,32 @@ class CommissionController(private val commissionRewardHandler: CommissionReward
         var createDate: Date
     )
 
+    @ApiOperation(
+        value = "Get all commissions by referral code",
+        notes = "Get all commissions by referral code."
+    )
+    @ApiResponse(
+        message = "OK",
+        code = 200,
+        examples = Example(
+            ExampleProperty(
+                value = """
+                    [
+                        {
+                            "rewardedUuid": "b3e4f2bd-15c6-4912-bdef-161445a98193",
+                            "referentUuid": "a5e510f9-bda8-4ecb-b500-0980f525dc52",
+                            "referralCode": "10000",
+                            "richTrade": 1,
+                            "referentOrderDirection": BID,
+                            "share": 0.01,
+                            "createDate": 1646213088
+                        }
+                    ]
+                """,
+                mediaType = "application/json"
+            )
+        )
+    )
     @GetMapping("/commissions/{code}")
     suspend fun getCommissionsByReferrerAndCode(
         @PathVariable code: String
@@ -36,8 +66,34 @@ class CommissionController(private val commissionRewardHandler: CommissionReward
         }
     }
 
+    @ApiOperation(
+        value = "Get all commissions",
+        notes = "Get all commissions by referer or referent."
+    )
+    @ApiResponse(
+        message = "OK",
+        code = 200,
+        examples = Example(
+            ExampleProperty(
+                value = """
+                    [
+                        {
+                            "rewardedUuid": "b3e4f2bd-15c6-4912-bdef-161445a98193",
+                            "referentUuid": "a5e510f9-bda8-4ecb-b500-0980f525dc52",
+                            "referralCode": "10000",
+                            "richTrade": 1,
+                            "referentOrderDirection": BID,
+                            "share": 0.01,
+                            "createDate": 1646213088
+                        }
+                    ]
+                """,
+                mediaType = "application/json"
+            )
+        )
+    )
     @GetMapping("/commissions")
-    suspend fun getCommissionsByReferent(
+    suspend fun getCommissions(
         @RequestParam rewardedUuid: String?,
         @RequestParam referentUuid: String?
     ): List<CommissionRewardBody> {
@@ -54,6 +110,11 @@ class CommissionController(private val commissionRewardHandler: CommissionReward
         }
     }
 
+    @ApiOperation(
+        value = "Batch delete commissions",
+        notes = "Delete commissions base on given information."
+    )
+    @ApiResponse(message = "OK", code = 200)
     @DeleteMapping("/commissions")
     suspend fun deleteCommissions(
         @RequestParam code: String?,
@@ -63,6 +124,11 @@ class CommissionController(private val commissionRewardHandler: CommissionReward
         commissionRewardHandler.deleteCommissions(code, referrerUuid, referentUuid)
     }
 
+    @ApiOperation(
+        value = "Delete commission record",
+        notes = "Delete commission record by id."
+    )
+    @ApiResponse(message = "OK", code = 200)
     @DeleteMapping("/commissions/{id}")
     suspend fun deleteCommissionById(@PathVariable id: Long) {
         commissionRewardHandler.deleteCommissionById(id)
