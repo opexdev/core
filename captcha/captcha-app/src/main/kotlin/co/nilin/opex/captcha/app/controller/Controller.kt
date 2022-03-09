82dc9cd63a6fbe98347388ee3a19650e557e066e
metalicn20 <metalicn20@gmail.com>
2022-03-09 23:59:03 +0330

    captcha: Change captcha verify controller

--- captcha/captcha-app/src/main/kotlin/co/nilin/opex/captcha/app/controller/Controller.kt
+++ captcha/captcha-app/src/main/kotlin/co/nilin/opex/captcha/app/controller/Controller.kt
@@ -37,7 +37,7 @@ class Controller(
         return ResponseEntity(image, HttpHeaders().apply { set("captcha-session-key", id) }, HttpStatus.OK)
     }
 
-    @ApiOperation(value = "Verify captcha", notes = "Verify captcha.")
+    @ApiOperation(value = "Verify captcha", notes = "Verify captcha. proof is a string in form of \"{{captcha-session-key}}:{{answer}}\"")
     @ApiResponses(
         ApiResponse(
             message = "OK",
@@ -50,12 +50,9 @@ class Controller(
     )
     @ResponseStatus(value = HttpStatus.NO_CONTENT)
     @DeleteMapping
-    suspend fun verifyCaptcha(
-        @RequestParam id: String,
-        @RequestParam answer: String
-    ) {
+    suspend fun verifyCaptcha(@RequestParam proof: String) {
         val ms = System.currentTimeMillis()
         store.filterValues { it <= ms - captchaWindowSeconds * 1000 }.forEach { store.remove(it.key) }
-        if (store.contains("$id:$answer".sha256())) store.remove(id) else throw OpexException(OpexError.NotFound)
+        proof.sha256().let { if (store.contains(it)) store.remove(it) else throw OpexException(OpexError.NotFound) }
     }
 }
