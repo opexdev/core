18412e1328ba36f47394766b367fc997620019ea
metalicn20 <metalicn20@gmail.com>
2022-05-11 16:45:13 +0430

    Remove redundant `data.sql`s

--- utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Market.kt
+++ utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Market.kt
@@ -4,6 +4,7 @@ import java.math.BigDecimal
 
 data class Market(
     var pair: String,
+    var aliases: List<String>,
     var leftSide: String,
     var rightSide: String,
     var leftSideFraction: BigDecimal?,
