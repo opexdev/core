45b45e8ba95527d87cba299e09fb25f7042ff7f5
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-12 16:41:51 +0430

    preferences: Fix default values

--- utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Currency.kt
+++ utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Currency.kt
@@ -6,12 +6,20 @@ data class Currency(
     var symbol: String = "",
     var name: String = "",
     var precision: BigDecimal = BigDecimal.ZERO,
-    var leftSideFraction: BigDecimal = precision,
-    var rightSideFraction: BigDecimal = precision,
     var mainBalance: BigDecimal = BigDecimal.ZERO,
     var dailyTotal: BigDecimal = BigDecimal.ZERO,
     var dailyCount: Int = 0,
     var monthlyTotal: BigDecimal = BigDecimal.ZERO,
     var monthlyCount: Int = 0,
     var implementations: List<CurrencyImplementation> = emptyList()
-)
+) {
+    var leftSideFraction: BigDecimal? = null
+        get() {
+            return field ?: precision
+        }
+
+    var rightSideFraction: BigDecimal? = null
+        get() {
+            return field ?: precision
+        }
+}
--- utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Market.kt
+++ utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Market.kt
@@ -5,9 +5,13 @@ import java.math.BigDecimal
 data class Market(
     var leftSide: String = "",
     var rightSide: String = "",
-    var pair: String? = "${leftSide}_$rightSide",
     var feeConfigs: List<FeeConfig> = emptyList(),
     var aliases: List<Alias> = emptyList(),
     var leftSideFraction: BigDecimal? = null,
     var rightSideFraction: BigDecimal? = null
-)
+) {
+    var pair: String? = null
+        get() {
+            return field ?: "${leftSide}_$rightSide"
+        }
+}
