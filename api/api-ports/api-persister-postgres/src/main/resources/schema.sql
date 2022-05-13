4ad4ea9279bfc72623c4a7970f02cb5b1a81543b
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-13 17:59:49 +0430

    api: Fix symbol_maps schema

--- api/api-ports/api-persister-postgres/src/main/resources/schema.sql
+++ api/api-ports/api-persister-postgres/src/main/resources/schema.sql
@@ -58,9 +58,9 @@ CREATE TABLE IF NOT EXISTS symbol_maps
 (
     id          SERIAL PRIMARY KEY,
     symbol      VARCHAR(72) NOT NULL,
-    aliasKey    VARCHAR(72) NOT NULL,
+    alias_key    VARCHAR(72) NOT NULL,
     alias       VARCHAR(72) NOT NULL,
-    UNIQUE (symbol, aliasKey, alias)
+    UNIQUE (symbol, alias_key, alias)
 );
 
 CREATE OR REPLACE FUNCTION interval_generator(
