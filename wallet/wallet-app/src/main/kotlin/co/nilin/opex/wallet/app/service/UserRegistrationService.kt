f370fd2dd6d5d4c4b2d74a6bde6bfcdbcc960076
metalicn20 <metalicn20@gmail.com>
2022-05-14 11:31:44 +0430

    wallet: Fix crash in user registration service

--- wallet/wallet-app/src/main/kotlin/co/nilin/opex/wallet/app/service/UserRegistrationService.kt
+++ wallet/wallet-app/src/main/kotlin/co/nilin/opex/wallet/app/service/UserRegistrationService.kt
@@ -8,29 +8,23 @@ import co.nilin.opex.wallet.core.spi.CurrencyService
 import co.nilin.opex.wallet.core.spi.WalletManager
 import co.nilin.opex.wallet.core.spi.WalletOwnerManager
 import co.nilin.opex.wallet.ports.kafka.listener.model.UserCreatedEvent
-import com.fasterxml.jackson.databind.ObjectMapper
-import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
-import org.springframework.beans.factory.annotation.Value
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Component
 import org.springframework.transaction.annotation.Transactional
-import java.io.File
 import java.math.BigDecimal
 
 @Component
 class UserRegistrationService(
-    val walletOwnerManager: WalletOwnerManager,
-    val walletManager: WalletManager,
-    val currencyService: CurrencyService,
-    @Value("\${app.preferences}") val file: File
+    val walletOwnerManager: WalletOwnerManager, val walletManager: WalletManager, val currencyService: CurrencyService
 ) {
+    @Autowired
+    private lateinit var preferences: ProjectPreferences
+
     @Transactional
     suspend fun registerNewUser(event: UserCreatedEvent) {
-        val mapper = ObjectMapper(YAMLFactory())
-        val p: ProjectPreferences = mapper.readValue(file, ProjectPreferences::class.java)
-
         val owner = walletOwnerManager.createWalletOwner(event.uuid, "${event.firstName} ${event.lastName}", "1")
 
-        p.currencies.filter { it.gift > BigDecimal.ZERO }.forEach {
+        preferences.currencies.filter { it.gift > BigDecimal.ZERO }.forEach {
             val currency = currencyService.getCurrency(it.symbol) ?: throw OpexException(OpexError.CurrencyNotFound)
             walletManager.createWallet(owner, Amount(currency, it.gift), currency, "main")
         }
