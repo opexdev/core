9f82e76bf8b80142dc3598bee8e95947fc6f76ca
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-16 21:49:12 +0430

    wallet: Add currency service tests

--- wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/app/CurrencyServiceTest.kt
+++ wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/app/CurrencyServiceTest.kt
@@ -2,22 +2,43 @@ package co.nilin.opex.wallet.app
 
 import co.nilin.opex.wallet.ports.postgres.dao.CurrencyRepository
 import co.nilin.opex.wallet.ports.postgres.impl.CurrencyServiceImpl
+import co.nilin.opex.wallet.ports.postgres.model.CurrencyModel
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 import org.mockito.Mock
 import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import reactor.core.publisher.Mono
 
 private class CurrencyServiceTest {
     @Mock
-    private lateinit var currencyRepository: CurrencyRepository
+    private var currencyRepository: CurrencyRepository
 
     private var currencyService: CurrencyServiceImpl
 
     init {
         MockitoAnnotations.openMocks(this)
+        currencyRepository = mock {
+            on {
+                findBySymbol("ETH")
+            } doReturn Mono.just(
+                CurrencyModel(
+                    "ETH",
+                    "Ethereum",
+                    0.0001
+                )
+            )
+        }
         currencyService = CurrencyServiceImpl(currencyRepository)
     }
 
     @Test
-    fun givenSymbol_whenGetCurrency_thenReturnCurrency(): Unit = runBlocking {}
+    fun givenSymbol_whenGetCurrency_thenReturnCurrency(): Unit = runBlocking {
+        val c = currencyService.getCurrency("ETH")
+
+        assertThat(c).isNotNull
+        assertThat(c!!.getSymbol()).isEqualTo("ETH")
+    }
 }
