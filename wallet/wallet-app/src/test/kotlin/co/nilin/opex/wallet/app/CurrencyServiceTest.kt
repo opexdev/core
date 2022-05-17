0bcef902174558bf07e230a7a729d4628e4a6856
metalicn20 <metalicn20@gmail.com>
2022-05-17 16:33:52 +0430

    wallet: Refactor currency service tests

--- wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/app/CurrencyServiceTest.kt
+++ wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/app/CurrencyServiceTest.kt
@@ -1,45 +1,10 @@
 package co.nilin.opex.wallet.app
 
-import co.nilin.opex.wallet.ports.postgres.dao.CurrencyRepository
-import co.nilin.opex.wallet.ports.postgres.impl.CurrencyServiceImpl
-import co.nilin.opex.wallet.ports.postgres.model.CurrencyModel
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
-import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.mock
-import reactor.core.publisher.Mono
 
-private class CurrencyServiceTest {
-    @Mock
-    private var currencyRepository: CurrencyRepository
-
-    private var currencyService: CurrencyServiceImpl
-
-    init {
-        MockitoAnnotations.openMocks(this)
-        currencyRepository = mock {
-            on {
-                findBySymbol("ETH")
-            } doReturn Mono.just(
-                CurrencyModel(
-                    "ETH",
-                    "Ethereum",
-                    0.0001
-                )
-            )
-            on {
-                findBySymbol("")
-            } doReturn Mono.empty()
-            on {
-                findBySymbol("WRONG")
-            } doReturn Mono.empty()
-        }
-        currencyService = CurrencyServiceImpl(currencyRepository)
-    }
-
+private class CurrencyServiceTest : CurrencyServiceTestBase() {
     @Test
     fun givenSymbol_whenGetCurrency_thenReturnCurrency(): Unit = runBlocking {
         val c = currencyService.getCurrency("ETH")
---
+++ wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/app/CurrencyServiceTestBase.kt
@@ -0,0 +1,39 @@
+package co.nilin.opex.wallet.app
+
+import co.nilin.opex.wallet.ports.postgres.dao.CurrencyRepository
+import co.nilin.opex.wallet.ports.postgres.impl.CurrencyServiceImpl
+import co.nilin.opex.wallet.ports.postgres.model.CurrencyModel
+import org.mockito.Mock
+import org.mockito.MockitoAnnotations
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.mock
+import reactor.core.publisher.Mono
+
+internal open class CurrencyServiceTestBase {
+    @Mock
+    protected var currencyRepository: CurrencyRepository
+
+    protected var currencyService: CurrencyServiceImpl
+
+    init {
+        MockitoAnnotations.openMocks(this)
+        currencyRepository = mock {
+            on {
+                findBySymbol("ETH")
+            } doReturn Mono.just(
+                CurrencyModel(
+                    "ETH",
+                    "Ethereum",
+                    0.0001
+                )
+            )
+            on {
+                findBySymbol("")
+            } doReturn Mono.empty()
+            on {
+                findBySymbol("WRONG")
+            } doReturn Mono.empty()
+        }
+        currencyService = CurrencyServiceImpl(currencyRepository)
+    }
+}
