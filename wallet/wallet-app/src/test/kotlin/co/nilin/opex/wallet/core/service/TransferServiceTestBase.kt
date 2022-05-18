9b6edc2c6cfc3f012731cc6fdb34645a639d13cb
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-18 23:52:46 +0430

    wallet: Improve transfer service tests

--- wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/core/service/TransferServiceTestBase.kt
+++ wallet/wallet-app/src/test/kotlin/co/nilin/opex/wallet/core/service/TransferServiceTestBase.kt
@@ -1,36 +1,20 @@
 package co.nilin.opex.wallet.core.service
 
-import co.nilin.opex.wallet.core.model.Amount
 import co.nilin.opex.wallet.core.model.Currency
-import co.nilin.opex.wallet.core.model.Wallet
 import co.nilin.opex.wallet.core.model.WalletOwner
 import co.nilin.opex.wallet.core.spi.TransactionManager
 import co.nilin.opex.wallet.core.spi.WalletListener
 import co.nilin.opex.wallet.core.spi.WalletManager
 import co.nilin.opex.wallet.core.spi.WalletOwnerManager
-import kotlinx.coroutines.runBlocking
-import org.mockito.ArgumentMatchers.anyString
-import org.mockito.Mock
-import org.mockito.MockitoAnnotations
-import org.mockito.kotlin.any
-import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import java.math.BigDecimal
 
 internal open class TransferServiceTestBase {
-    @Mock
-    protected var walletOwnerManager: WalletOwnerManager
-
-    @Mock
-    protected var walletManager: WalletManager
-
-    @Mock
-    protected var walletListener: WalletListener
-
-    @Mock
-    protected var transactionManager: TransactionManager
-
-    protected var transferService: TransferService
+    protected var walletOwnerManager: WalletOwnerManager = mock()
+    protected var walletManager: WalletManager = mock()
+    protected var walletListener: WalletListener = mock()
+    protected var transactionManager: TransactionManager = mock()
+    protected var transferService: TransferService =
+        TransferService(walletManager, walletListener, walletOwnerManager, transactionManager)
 
     protected val currency = object : Currency {
         override fun getSymbol() = "ETH"
@@ -46,34 +30,5 @@ internal open class TransferServiceTestBase {
         override fun isTradeAllowed() = true
         override fun isWithdrawAllowed() = true
         override fun isDepositAllowed() = true
-    }
-
-    init {
-        MockitoAnnotations.openMocks(this)
-        walletOwnerManager = mock {
-            on { runBlocking { isWithdrawAllowed(any(), any()) } } doReturn true
-            on { runBlocking { isDepositAllowed(any(), any()) } } doReturn true
-        }
-        walletManager = mock {
-            on { runBlocking { isWithdrawAllowed(any(), any()) } } doReturn true
-            on { runBlocking { isDepositAllowed(any(), any()) } } doReturn true
-            on { runBlocking { decreaseBalance(any(), any()) } } doReturn Unit
-            on { runBlocking { increaseBalance(any(), any()) } } doReturn Unit
-            on { runBlocking { findWalletById(20L) } } doReturn object : Wallet {
-                override fun id() = 20L
-                override fun owner() = walletOwner
-                override fun balance() = Amount(currency, BigDecimal.valueOf(1))
-                override fun currency() = currency
-                override fun type() = "main"
-            }
-        }
-        walletListener = mock {
-            on { runBlocking { onWithdraw(any(), any(), any(), anyString(), any()) } } doReturn Unit
-            on { runBlocking { onDeposit(any(), any(), any(), any(), anyString(), any()) } } doReturn Unit
-        }
-        transactionManager = mock {
-            on { runBlocking { save(any()) } } doReturn "1"
-        }
-        transferService = TransferService(walletManager, walletListener, walletOwnerManager, transactionManager)
     }
 }
