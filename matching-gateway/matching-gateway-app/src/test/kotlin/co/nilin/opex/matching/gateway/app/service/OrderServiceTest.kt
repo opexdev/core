8b07dd103fd70c363becc87c1f982c72743620a3
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-19 13:56:50 +0430

    matching-gateway: Improve tests

--- matching-gateway/matching-gateway-app/src/test/kotlin/co/nilin/opex/matching/gateway/app/service/OrderServiceTest.kt
+++ matching-gateway/matching-gateway-app/src/test/kotlin/co/nilin/opex/matching/gateway/app/service/OrderServiceTest.kt
@@ -1,39 +1,30 @@
 package co.nilin.opex.matching.gateway.app.service
 
-import co.nilin.opex.matching.engine.core.eventh.events.CancelOrderEvent
 import co.nilin.opex.matching.engine.core.model.MatchConstraint
 import co.nilin.opex.matching.engine.core.model.OrderDirection
 import co.nilin.opex.matching.engine.core.model.OrderType
-import co.nilin.opex.matching.engine.core.model.Pair
 import co.nilin.opex.matching.gateway.app.inout.CancelOrderRequest
 import co.nilin.opex.matching.gateway.app.inout.CreateOrderRequest
 import co.nilin.opex.matching.gateway.app.inout.PairConfig
 import co.nilin.opex.matching.gateway.app.inout.PairFeeConfig
-import co.nilin.opex.matching.gateway.ports.kafka.submitter.inout.OrderSubmitRequest
 import co.nilin.opex.matching.gateway.ports.kafka.submitter.inout.OrderSubmitResult
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
 import org.mockito.kotlin.doReturn
-import org.mockito.kotlin.eq
 import org.mockito.kotlin.stubbing
-import org.mockito.kotlin.whenever
 import java.math.BigDecimal
 
 private class OrderServiceTest : OrderServiceTestBase() {
     @Test
     fun givenLimitASKOrder_whenSubmitNewOrder_thenOrderSubmitResult(): Unit = runBlocking {
+        val pairConfig = PairConfig("ETH_USDT", "ETH", "USDT", 0.01, 0.0001)
         stubbing(pairConfigLoader) {
-            onBlocking { load("ETH_USDT", OrderDirection.ASK, "1") } doReturn PairFeeConfig(
-                PairConfig(
-                    "ETH_USDT",
-                    "ETH",
-                    "USDT",
-                    0.0001,
-                    0.01
-                ),
+            onBlocking { load("ETH_USDT", OrderDirection.ASK, "") } doReturn PairFeeConfig(
+                pairConfig,
                 "ASK",
-                "1",
+                "",
                 0.01,
                 0.01
             )
@@ -41,7 +32,7 @@ private class OrderServiceTest : OrderServiceTestBase() {
         val order = CreateOrderRequest(
             "a2930d06-0c84-4448-bff7-65134184bb1d",
             "ETH_USDT",
-            BigDecimal.valueOf(100),
+            BigDecimal.valueOf(100000),
             BigDecimal.valueOf(0.001),
             OrderDirection.ASK,
             MatchConstraint.GTC,
@@ -49,27 +40,17 @@ private class OrderServiceTest : OrderServiceTestBase() {
         )
         stubbing(accountantApiProxy) {
             onBlocking {
-                canCreateOrder(order.uuid!!, "ETH_USDT", order.quantity)
+                canCreateOrder(order.uuid!!, "ETH", order.quantity)
             } doReturn true
         }
         stubbing(orderSubmitter) {
             onBlocking {
-                submit(
-                    eq(
-                        OrderSubmitRequest(
-                            order.uuid!!,
-                            Pair("ETH", "USDT"),
-                            (order.price / BigDecimal.valueOf(0.01)).longValueExact(),
-                            (order.quantity / BigDecimal.valueOf(0.0001)).longValueExact(),
-                            order.direction,
-                            order.matchConstraint,
-                            order.orderType
-                        )
-                    )
-                )
+                submit(any())
             } doReturn OrderSubmitResult(null)
         }
-        whenever(kafkaHealthIndicator.isHealthy).thenReturn(true)
+        stubbing(kafkaHealthIndicator) {
+            on { isHealthy } doReturn true
+        }
 
         val orderSubmitResult = orderService.submitNewOrder(order)
 
@@ -86,7 +67,7 @@ private class OrderServiceTest : OrderServiceTestBase() {
         )
         stubbing(eventSubmitter) {
             onBlocking {
-                submit(CancelOrderEvent(order.ouid, order.uuid, order.orderId, Pair("ETH", "USDT")))
+                submit(any())
             } doReturn OrderSubmitResult(null)
         }
 
