19ad3c26d8cdaa8642af9ed64d9821ea38ba01dc
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-21 21:59:10 +0430

    api: Add market query handler tests

--- api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/MarketQueryHandlerTest.kt
+++ api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/MarketQueryHandlerTest.kt
@@ -1,3 +1,45 @@
 package co.nilin.opex.api.ports.postgres.impl
 
-class MarketQueryHandlerTest
+import co.nilin.opex.api.core.spi.SymbolMapper
+import co.nilin.opex.api.ports.postgres.dao.OrderRepository
+import co.nilin.opex.api.ports.postgres.dao.OrderStatusRepository
+import co.nilin.opex.api.ports.postgres.dao.TradeRepository
+import kotlinx.coroutines.runBlocking
+import org.junit.jupiter.api.Test
+import org.mockito.kotlin.mock
+import java.security.Principal
+
+class MarketQueryHandlerTest {
+    private val orderRepository: OrderRepository = mock()
+    private val tradeRepository: TradeRepository = mock()
+    private val orderStatusRepository: OrderStatusRepository = mock()
+    private val symbolMapper: SymbolMapper = mock()
+    private val marketQueryHandler =
+        MarketQueryHandlerImpl(orderRepository, tradeRepository, orderStatusRepository, symbolMapper)
+
+    @Test
+    fun givenSymbol_whenOpenASKOrders_thenReturnOrderBookResponseList(): Unit = runBlocking {
+        marketQueryHandler.openAskOrders("ETH_USDT", 10)
+    }
+
+    @Test
+    fun givenSymbol_whenOpenBIDOrders_thenReturnOrderBookResponseList(): Unit = runBlocking {
+        marketQueryHandler.openBidOrders("ETH_USDT", 10)
+    }
+
+    @Test
+    fun givenSymbol_whenLastOrder_thenReturnQueryOrderResponse(): Unit = runBlocking {
+        marketQueryHandler.lastOrder("ETH_USDT")
+    }
+
+    @Test
+    fun givenSymbol_whenLastPrice_thenPriceTickerResponse(): Unit = runBlocking {
+        marketQueryHandler.lastPrice("ETH_USDT")
+    }
+
+    @Test
+    fun givenSymbol_whenRecentTrades_thenMarketTradeResponseFlow(): Unit = runBlocking {
+        marketQueryHandler.recentTrades("ETH_USDT", 10)
+    }
+}
+
