39e9874f3a5300c0c06c7928d6df7825f4e0672f
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-05-21 23:32:28 +0430

    api: Add more order persister tests

--- api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/OrderPersisterTest.kt
+++ api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/OrderPersisterTest.kt
@@ -8,11 +8,19 @@ import co.nilin.opex.api.core.inout.OrderDirection
 import co.nilin.opex.api.core.inout.OrderStatus
 import co.nilin.opex.api.ports.postgres.dao.OrderRepository
 import co.nilin.opex.api.ports.postgres.dao.OrderStatusRepository
+import co.nilin.opex.api.ports.postgres.model.OrderModel
+import co.nilin.opex.api.ports.postgres.model.OrderStatusModel
 import kotlinx.coroutines.runBlocking
+import org.assertj.core.api.Assertions.assertThatNoException
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
-import org.assertj.core.api.Assertions.*
+import org.mockito.kotlin.stubbing
+import reactor.core.publisher.Mono
 import java.math.BigDecimal
+import java.time.LocalDateTime
+import java.time.ZoneOffset
 
 class OrderPersisterTest {
     private val orderRepository: OrderRepository = mock()
@@ -41,8 +49,49 @@ class OrderPersisterTest {
             BigDecimal.valueOf(0), // ?
             0
         )
+        stubbing(orderRepository) {
+            on {
+                save(any())
+            } doReturn Mono.just(
+                OrderModel(
+                    1,
+                    "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                    "18013d13-0568-496b-b93b-2524c8132b93",
+                    "id", // ?
+                    "ETH_USDT",
+                    1,
+                    0.01,
+                    0.01,
+                    0.0001,
+                    0.01,
+                    "1",
+                    OrderDirection.ASK,
+                    MatchConstraint.GTC,
+                    MatchingOrderType.LIMIT_ORDER,
+                    100000.0,
+                    0.01,
+                    0.0, // ?
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC),
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                )
+            )
+        }
+        stubbing(orderStatusRepository) {
+            on {
+                save(any())
+            } doReturn Mono.just(
+                OrderStatusModel(
+                    "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                    0.0, // ?
+                    0.0, // ?
+                    0, // ?
+                    0, // ?
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                )
+            )
+        }
 
-        assertThatThrownBy { runBlocking { orderPersister.save(richOrder) } }.doesNotThrowAnyException()
+        assertThatNoException().isThrownBy { runBlocking { orderPersister.save(richOrder) } }
     }
 
     @Test
@@ -54,7 +103,21 @@ class OrderPersisterTest {
             BigDecimal.valueOf(0.08),
             OrderStatus.PARTIALLY_FILLED
         )
+        stubbing(orderStatusRepository) {
+            on {
+                save(any())
+            } doReturn Mono.just(
+                OrderStatusModel(
+                    "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                    0.0, // ?
+                    0.0, // ?
+                    0, // ?
+                    0, // ?
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                )
+            )
+        }
 
-        assertThatThrownBy { runBlocking { orderPersister.update(richOrderUpdate) } }.doesNotThrowAnyException()
+        assertThatNoException().isThrownBy { runBlocking { orderPersister.update(richOrderUpdate) } }
     }
 }
