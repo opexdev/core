7da068c288b0e8dfdc1e623a1f124a3c92422f8e
metalicn20 <metalicn20@gmail.com>
2022-05-22 13:29:45 +0430

    api: Add stubbing methods

--- api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/TradePersisterTest.kt
+++ api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/TradePersisterTest.kt
@@ -3,10 +3,15 @@ package co.nilin.opex.api.ports.postgres.impl
 import co.nilin.opex.api.core.event.RichTrade
 import co.nilin.opex.api.core.inout.OrderDirection
 import co.nilin.opex.api.ports.postgres.dao.TradeRepository
+import co.nilin.opex.api.ports.postgres.model.TradeModel
 import kotlinx.coroutines.runBlocking
 import org.assertj.core.api.Assertions.assertThatNoException
 import org.junit.jupiter.api.Test
+import org.mockito.kotlin.any
+import org.mockito.kotlin.doReturn
 import org.mockito.kotlin.mock
+import org.mockito.kotlin.stubbing
+import reactor.core.publisher.Mono
 import java.math.BigDecimal
 import java.time.LocalDateTime
 import java.time.ZoneOffset
@@ -43,6 +48,30 @@ class TradePersisterTest {
             BigDecimal.valueOf(0), // ?
             LocalDateTime.ofEpochSecond(1653125640, 0, ZoneOffset.UTC)
         )
+        stubbing(tradeRepository) {
+            on {
+                save(any())
+            } doReturn Mono.just(
+                TradeModel(
+                    1,
+                    1,
+                    "ETH_USDT",
+                    0.001,
+                    100000.0,
+                    100000.0,
+                    0.001,
+                    0.001,
+                    "",
+                    "",
+                    LocalDateTime.ofEpochSecond(1653125640, 0, ZoneOffset.UTC),
+                    "99289106-2775-44d4-bffc-ca35fc25e58c",
+                    "2fa73fa2-6d70-44b8-8571-e2b24e2eea2b",
+                    "52c6d890-3dd4-4fa8-9425-d9e0d6274705",
+                    "07bb979a-dfca-475b-a38b-fcc5dd2f88d8",
+                    LocalDateTime.ofEpochSecond(1653125640, 0, ZoneOffset.UTC)
+                )
+            )
+        }
 
         assertThatNoException().isThrownBy { runBlocking { tradePersister.save(richOrder) } }
     }
--- api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/UserQueryHandlerTest.kt
+++ api/api-app/src/test/kotlin/co/nilin/opex/api/ports/postgres/impl/UserQueryHandlerTest.kt
@@ -4,8 +4,12 @@ import co.nilin.opex.api.core.inout.*
 import co.nilin.opex.api.ports.postgres.dao.OrderRepository
 import co.nilin.opex.api.ports.postgres.dao.OrderStatusRepository
 import co.nilin.opex.api.ports.postgres.dao.TradeRepository
+import co.nilin.opex.api.ports.postgres.model.OrderModel
+import co.nilin.opex.api.ports.postgres.model.OrderStatusModel
+import co.nilin.opex.api.ports.postgres.model.TradeModel
 import kotlinx.coroutines.flow.count
 import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.runBlocking
 import org.junit.jupiter.api.Test
 import org.mockito.kotlin.mock
@@ -14,6 +18,9 @@ import java.time.LocalDateTime
 import java.time.ZoneOffset
 import java.util.*
 import org.assertj.core.api.Assertions.*
+import org.mockito.kotlin.doReturn
+import org.mockito.kotlin.stubbing
+import reactor.core.publisher.Mono
 import java.math.BigDecimal
 
 class UserQueryHandlerTest {
@@ -31,6 +38,40 @@ class UserQueryHandlerTest {
             Date.from(LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC).toInstant(ZoneOffset.UTC)),
             500
         )
+        stubbing(orderRepository) {
+            on {
+                findByUuidAndSymbolAndTimeBetween(
+                    principal.name,
+                    allOrderRequest.symbol,
+                    allOrderRequest.startTime,
+                    allOrderRequest.endTime
+                )
+            } doReturn flow {
+                emit(
+                    OrderModel(
+                        1,
+                        "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                        "18013d13-0568-496b-b93b-2524c8132b93",
+                        "id", // ?
+                        "ETH_USDT",
+                        1,
+                        0.01,
+                        0.01,
+                        0.0001,
+                        0.01,
+                        "1",
+                        OrderDirection.ASK,
+                        MatchConstraint.GTC,
+                        MatchingOrderType.LIMIT_ORDER,
+                        100000.0,
+                        0.01,
+                        0.0, // ?
+                        LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC),
+                        LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                    )
+                )
+            }
+        }
 
         val queryOrderResponses = userQueryHandler.allOrders(principal, allOrderRequest)
 
@@ -70,6 +111,38 @@ class UserQueryHandlerTest {
             Date.from(LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC).toInstant(ZoneOffset.UTC)),
             500
         )
+        stubbing(tradeRepository) {
+            on {
+                findByUuidAndSymbolAndTimeBetweenAndTradeIdGreaterThan(
+                    principal.name,
+                    tradeRequest.symbol,
+                    1,
+                    tradeRequest.startTime,
+                    tradeRequest.endTime
+                )
+            } doReturn flow {
+                emit(
+                    TradeModel(
+                        1,
+                        1,
+                        "ETH_USDT",
+                        0.001,
+                        100000.0,
+                        100000.0,
+                        0.001,
+                        0.001,
+                        "",
+                        "",
+                        LocalDateTime.ofEpochSecond(1653125640, 0, ZoneOffset.UTC),
+                        "99289106-2775-44d4-bffc-ca35fc25e58c",
+                        "2fa73fa2-6d70-44b8-8571-e2b24e2eea2b",
+                        "52c6d890-3dd4-4fa8-9425-d9e0d6274705",
+                        "07bb979a-dfca-475b-a38b-fcc5dd2f88d8",
+                        LocalDateTime.ofEpochSecond(1653125640, 0, ZoneOffset.UTC)
+                    )
+                )
+            }
+        }
 
         val tradeResponses = userQueryHandler.allTrades(principal, tradeRequest)
 
@@ -79,6 +152,36 @@ class UserQueryHandlerTest {
 
     @Test
     fun given_whenOpenOrders_then(): Unit = runBlocking {
+        stubbing(orderRepository) {
+            on {
+                findByUuidAndSymbolAndStatus(principal.name, "ETH_USDT", listOf(0))
+            } doReturn flow {
+                emit(
+                    OrderModel(
+                        1,
+                        "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                        "18013d13-0568-496b-b93b-2524c8132b93",
+                        "id", // ?
+                        "ETH_USDT",
+                        1,
+                        0.01,
+                        0.01,
+                        0.0001,
+                        0.01,
+                        "1",
+                        OrderDirection.ASK,
+                        MatchConstraint.GTC,
+                        MatchingOrderType.LIMIT_ORDER,
+                        100000.0,
+                        0.01,
+                        0.0, // ?
+                        LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC),
+                        LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                    )
+                )
+            }
+        }
+
         val queryOrderResponses = userQueryHandler.openOrders(principal, "ETH_USDT")
 
         assertThat(queryOrderResponses).isNotNull
@@ -92,6 +195,72 @@ class UserQueryHandlerTest {
             1,
             "2" // ?
         )
+        stubbing(orderRepository) {
+            on {
+                findBySymbolAndClientOrderId("ETH_USDT", "id")
+            } doReturn Mono.just(
+                OrderModel(
+                    1,
+                    "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                    "18013d13-0568-496b-b93b-2524c8132b93",
+                    "id", // ?
+                    "ETH_USDT",
+                    1,
+                    0.01,
+                    0.01,
+                    0.0001,
+                    0.01,
+                    "1",
+                    OrderDirection.ASK,
+                    MatchConstraint.GTC,
+                    MatchingOrderType.LIMIT_ORDER,
+                    100000.0,
+                    0.01,
+                    0.0, // ?
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC),
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                )
+            )
+            on {
+                findBySymbolAndOrderId("ETH_USDT", 1)
+            } doReturn Mono.just(
+                OrderModel(
+                    1,
+                    "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                    "18013d13-0568-496b-b93b-2524c8132b93",
+                    "id", // ?
+                    "ETH_USDT",
+                    1,
+                    0.01,
+                    0.01,
+                    0.0001,
+                    0.01,
+                    "1",
+                    OrderDirection.ASK,
+                    MatchConstraint.GTC,
+                    MatchingOrderType.LIMIT_ORDER,
+                    100000.0,
+                    0.01,
+                    0.0, // ?
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC),
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                )
+            )
+        }
+        stubbing(orderStatusRepository) {
+            on {
+                findMostRecentByOUID("f1167d30-ccc0-4f86-ab5d-dd24aa3250df")
+            } doReturn Mono.just(
+                OrderStatusModel(
+                    "f1167d30-ccc0-4f86-ab5d-dd24aa3250df",
+                    0.0, // ?
+                    0.0, // ?
+                    0, // ?
+                    0, // ?
+                    LocalDateTime.ofEpochSecond(1653125840, 0, ZoneOffset.UTC)
+                )
+            )
+        }
 
         val queryOrderResponse = userQueryHandler.queryOrder(principal, queryOrderRequest)
         assertThat(queryOrderResponse).isNotNull
