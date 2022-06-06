70a4592f617d4c59ed4ae1d18b8fe795a73061b4
ebrahimmfadae <ebrahimmfadae@gamil.com>
2022-06-06 13:19:07 +0430

    preferences: Update preferences

--- utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Chain.kt
+++ utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/Chain.kt
@@ -2,7 +2,5 @@ package co.nilin.opex.utility.preferences
 
 data class Chain(
     var name: String = "",
-    var addressType: String = "",
-    val endpointUrl: String = "",
-    var schedule: ChainSyncSchedule = ChainSyncSchedule()
+    var addressType: String = ""
 )
--- utility/preferences/src/main/kotlin/co/nilin/opex/utility/preferences/ChainSyncSchedule.kt
+++
@@ -1,5 +0,0 @@
-package co.nilin.opex.utility.preferences
-
-data class ChainSyncSchedule(
-    var delay: Long = 600, var errorDelay: Long = 60
-)
