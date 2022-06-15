828c9817ee385cb1eabbde4b745e9e3f2a900b81
ebrahimmfadae <ebrahimmfadae@gmail.com>
2022-06-15 15:45:43 +0430

    bc-gateway: Revert removing deposits

--- bc-gateway/bc-gateway-core/src/main/kotlin/co/nilin/opex/bcgateway/core/service/WalletSyncServiceImpl.kt
+++ bc-gateway/bc-gateway-core/src/main/kotlin/co/nilin/opex/bcgateway/core/service/WalletSyncServiceImpl.kt
@@ -2,38 +2,64 @@ package co.nilin.opex.bcgateway.core.service
 
 import co.nilin.opex.bcgateway.core.api.WalletSyncService
 import co.nilin.opex.bcgateway.core.model.CurrencyImplementation
+import co.nilin.opex.bcgateway.core.model.Deposit
 import co.nilin.opex.bcgateway.core.model.Transfer
 import co.nilin.opex.bcgateway.core.spi.AssignedAddressHandler
 import co.nilin.opex.bcgateway.core.spi.CurrencyHandler
+import co.nilin.opex.bcgateway.core.spi.DepositHandler
 import co.nilin.opex.bcgateway.core.spi.WalletProxy
 import co.nilin.opex.bcgateway.core.utils.LoggerDelegate
 import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
 import kotlinx.coroutines.coroutineScope
+import kotlinx.coroutines.flow.asFlow
+import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.toList
 import org.slf4j.Logger
 import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
 import java.math.BigDecimal
 
 @Service
 class WalletSyncServiceImpl(
     private val walletProxy: WalletProxy,
     private val assignedAddressHandler: AssignedAddressHandler,
-    private val currencyHandler: CurrencyHandler
+    private val currencyHandler: CurrencyHandler,
+    private val depositHandler: DepositHandler
 ) : WalletSyncService {
 
     private val logger: Logger by LoggerDelegate()
 
-    override suspend fun syncTransfers(transfers: List<Transfer>): Unit = coroutineScope {
-        transfers.map { transfer ->
+    @Transactional
+    override suspend fun syncTransfers(transfers: List<Transfer>) = coroutineScope {
+        val deposits = transfers.asFlow().map {
             async {
-                assignedAddressHandler.findUuid(transfer.receiver.address, transfer.receiver.memo)?.also { uuid ->
-                    logger.info("Deposit came for $uuid - to ${transfer.receiver.address}")
-                    currencyHandler.findByChainAndTokenAddress(transfer.chain, transfer.tokenAddress)?.also { symbol ->
-                        sendDeposit(uuid, symbol, transfer)
-                    }
+                val uuid = async {
+                    assignedAddressHandler.findUuid(it.receiver.address, it.receiver.memo)
+                        ?: throw IllegalStateException("Receiver not found")
                 }
+                val currencyImpl = async {
+                    currencyHandler.findByChainAndTokenAddress(it.chain, it.tokenAddress)
+                        ?: throw IllegalStateException("Currency implementation not found")
+                }
+                sendDeposit(uuid.await(), currencyImpl.await(), it)
+                logger.info("Deposit synced for $uuid on ${currencyImpl.await().currency.symbol} - to ${it.receiver.address}")
+                it
             }
-        }.awaitAll()
+        }.map {
+            it.await()
+        }.map {
+            Deposit(
+                null,
+                it.txHash,
+                it.receiver.address,
+                it.receiver.memo,
+                it.amount,
+                it.chain,
+                it.isTokenTransfer,
+                it.tokenAddress
+            )
+        }.toList()
+        depositHandler.saveAll(deposits)
     }
 
     private suspend fun sendDeposit(uuid: String, currencyImpl: CurrencyImplementation, transfer: Transfer) {
