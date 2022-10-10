66f5a1deca3c730e7e7120aaf7804c4a835d9c7d
Peyman <marchosiax@gmail.com>
2022-10-10 16:08:15 +0330

    Add logging in whitelist config

--- user-management/keycloak-gateway/src/main/kotlin/co/nilin/opex/auth/gateway/config/WhitelistConfig.kt
+++ user-management/keycloak-gateway/src/main/kotlin/co/nilin/opex/auth/gateway/config/WhitelistConfig.kt
@@ -2,6 +2,7 @@ package co.nilin.opex.auth.gateway.config
 
 import co.nilin.opex.auth.gateway.data.Whitelist
 import co.nilin.opex.utility.preferences.Preferences
+import org.slf4j.LoggerFactory
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import java.io.File
@@ -9,13 +10,29 @@ import java.io.File
 @Configuration
 class WhitelistConfig(private val preferences: Preferences) {
 
+    private val logger = LoggerFactory.getLogger(WhitelistConfig::class.java)
+
     @Bean("whitelist")
     fun whitelist(): Whitelist {
-        val whitelist = preferences.auth.whitelist
-        val file = File(whitelist.file)
-        if (!whitelist.enabled || !file.exists()) return Whitelist()
-        val list = file.readLines().onEach { it.trim().toLowerCase() }
-        return Whitelist(list.isNotEmpty(), list)
+        val whitelist = with(preferences.auth.whitelist) {
+            val file = File(file)
+            if (!enabled) {
+                logger.info("whitelist disabled by preferences")
+                Whitelist()
+            }
+
+            if (!file.exists()) {
+                logger.info("whitelist file doesn't exists")
+                Whitelist()
+            }
+
+            val list = file.readLines().onEach { it.trim().toLowerCase() }
+            Whitelist(list.isNotEmpty(), list)
+        }
+
+        logger.info("whitelist enabled: ${whitelist.isEnabled}")
+        logger.info("whitelist emails: ${whitelist.emails}")
+        return whitelist
     }
 
 }
