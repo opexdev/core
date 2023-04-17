43314dd17e620b53b319a44383a1acdc44145b30
Peyman <marchosiax@gmail.com>
2023-04-17 14:44:12 +0330

    Finish setting up prometheus/grafana and elk stack (#348)
    * Add logstash and configs
    
    * Update elk versions
    
    * Add prometheus and grafana
    
    * Setup elk security

--- captcha/captcha-app/src/main/kotlin/co/nilin/opex/captcha/app/App.kt
+++ captcha/captcha-app/src/main/kotlin/co/nilin/opex/captcha/app/App.kt
@@ -4,10 +4,12 @@ import co.nilin.opex.utility.error.EnableOpexErrorHandler
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.ComponentScan
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
 @ComponentScan("co.nilin.opex")
 @EnableOpexErrorHandler
+@EnableScheduling
 class App
 
 fun main(args: Array<String>) {
