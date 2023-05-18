16c342a408eb4f655e1013301552b9970f4d5764
maryarm <45322329+maryarm@users.noreply.github.com>
2023-05-18 16:23:28 +0200

    fixed #357: Checks for parent status during sync of financial actions

--- accountant/accountant-core/src/main/kotlin/co/nilin/opex/accountant/core/spi/FinancialActionLoader.kt
+++ accountant/accountant-core/src/main/kotlin/co/nilin/opex/accountant/core/spi/FinancialActionLoader.kt
@@ -6,4 +6,6 @@ interface FinancialActionLoader {
     suspend fun findLast(userUuid: String, ouid: String): FinancialAction?
     suspend fun loadUnprocessed(offset: Long, size: Long): List<FinancialAction>
     suspend fun countUnprocessed(userUuid: String, symbol: String, eventType: String): Long
+    suspend fun loadReadyToProcess(offset: Long, size: Long): List<FinancialAction>
+    suspend fun loadFinancialAction(id: Long?): FinancialAction?
 }
