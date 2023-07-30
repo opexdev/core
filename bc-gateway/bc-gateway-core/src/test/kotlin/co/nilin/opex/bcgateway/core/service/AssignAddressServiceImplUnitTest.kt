d17ef2c30ba686bf9164bcc9e29cf4f42554ce5e
Peyman <marchosiax@gmail.com>
2023-07-30 12:54:05 +0330

    Fix test failure

--- bc-gateway/bc-gateway-core/src/test/kotlin/co/nilin/opex/bcgateway/core/service/AssignAddressServiceImplUnitTest.kt
+++ bc-gateway/bc-gateway-core/src/test/kotlin/co/nilin/opex/bcgateway/core/service/AssignAddressServiceImplUnitTest.kt
@@ -5,147 +5,93 @@ import co.nilin.opex.bcgateway.core.model.Currency
 import co.nilin.opex.bcgateway.core.spi.AssignedAddressHandler
 import co.nilin.opex.bcgateway.core.spi.CurrencyHandler
 import co.nilin.opex.bcgateway.core.spi.ReservedAddressHandler
+import io.mockk.coEvery
+import io.mockk.mockk
 import kotlinx.coroutines.runBlocking
-import org.junit.jupiter.api.Assertions
 import org.junit.jupiter.api.Test
-import org.mockito.Mock
-import org.mockito.Mockito
-import org.mockito.MockitoAnnotations
+import org.assertj.core.api.Assertions.assertThat
 import java.math.BigDecimal
 import java.util.*
 
 class AssignAddressServiceImplUnitTest {
-    @Mock
-    lateinit var currencyHandler: CurrencyHandler
 
-    @Mock
-    lateinit var assignedAddressHandler: AssignedAddressHandler
+    private val currencyHandler = mockk<CurrencyHandler>()
+    private val assignedAddressHandler = mockk<AssignedAddressHandler>()
+    private val reservedAddressHandler = mockk<ReservedAddressHandler>()
 
-    @Mock
-    lateinit var reservedAddressHandler: ReservedAddressHandler
+    private val assignAddressServiceImpl =
+        AssignAddressServiceImpl(currencyHandler, assignedAddressHandler, reservedAddressHandler)
 
-    val assignAddressServiceImpl: AssignAddressServiceImpl
-
-    val currency = Currency("ETH", "Ethereum")
-    val chain = "Ethereum"
-    val ethAddressType = AddressType(1, "ETH", "+*", ".*")
-    val ethMemoAddressType = AddressType(2, "ETH", "+*", "+*")
-    val ethChain = Chain("ETH_MAINNET", arrayListOf(ethAddressType))
-    val bscChain = Chain("BSC_MAINNET", arrayListOf(ethAddressType, ethMemoAddressType))
+    private val currency = Currency("ETH", "Ethereum")
+    private val chain = "ETH_MAINNET"
+    private val ethAddressType = AddressType(1, "ETH", "+*", ".*")
+    private val ethMemoAddressType = AddressType(2, "ETH", "+*", "+*")
+    private val ethChain = Chain("ETH_MAINNET", arrayListOf(ethAddressType))
+    private val bscChain = Chain("BSC_MAINNET", arrayListOf(ethAddressType, ethMemoAddressType))
 
 
     init {
-        MockitoAnnotations.openMocks(this)
-        assignAddressServiceImpl = AssignAddressServiceImpl(
-            currencyHandler, assignedAddressHandler, reservedAddressHandler
+        val eth = CurrencyImplementation(
+            currency,
+            currency,
+            ethChain,
+            false,
+            null,
+            null,
+            true,
+            BigDecimal.ONE,
+            BigDecimal.TEN,
+            18
         )
-        runBlocking {
-            val eth = CurrencyImplementation(
-                currency,
-                currency,
-                ethChain,
-                false,
-                null,
-                null,
-                true,
-                BigDecimal.ONE,
-                BigDecimal.TEN,
-                18
-            )
 
-            val wrappedEth = CurrencyImplementation(
-                currency,
-                currency,
-                bscChain,
-                false,
-                "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
-                "WETH",
-                true,
-                BigDecimal.ONE,
-                BigDecimal.ONE,
-                18
-            )
+        val wrappedEth = CurrencyImplementation(
+            currency,
+            currency,
+            bscChain,
+            false,
+            "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",
+            "WETH",
+            true,
+            BigDecimal.ONE,
+            BigDecimal.ONE,
+            18
+        )
 
-            Mockito.`when`(currencyHandler.fetchCurrencyInfo(currency.symbol))
-                .thenReturn(CurrencyInfo(currency, listOf(eth, wrappedEth)))
-        }
+        coEvery { currencyHandler.fetchCurrencyInfo(currency.symbol) } returns CurrencyInfo(
+            currency,
+            listOf(eth, wrappedEth)
+        )
 
-
+        coEvery { assignedAddressHandler.persist(any()) } returns Unit
+        coEvery { reservedAddressHandler.remove(any()) } returns Unit
     }
 
     @Test
-    fun givenReservedAddressAndUserWithNoAssignedAddress_whenAssignAddress_thenReservedAddressAssigned() {
+    fun givenReservedAddressAndUserWithNoAssignedAddress_whenAssignAddress_thenReservedAddressAssigned(): Unit =
         runBlocking {
             val user = UUID.randomUUID().toString()
-            Mockito.`when`(
+            coEvery {
                 assignedAddressHandler.fetchAssignedAddresses(
-                    user,
-                    listOf(ethAddressType, ethMemoAddressType)
+                    eq(user),
+                    any()
                 )
-            ).thenReturn(
-                emptyList()
+            } returns emptyList()
+
+            coEvery { reservedAddressHandler.peekReservedAddress(ethAddressType) } returns ReservedAddress(
+                "0x1",
+                null,
+                ethAddressType
             )
-            Mockito.`when`(reservedAddressHandler.peekReservedAddress(ethAddressType)).thenReturn(
-                ReservedAddress("0x1", null, ethAddressType)
+
+            coEvery { reservedAddressHandler.peekReservedAddress(ethMemoAddressType) } returns ReservedAddress(
+                "0x2",
+                "Memo",
+                ethMemoAddressType
             )
-            Mockito.`when`(reservedAddressHandler.peekReservedAddress(ethMemoAddressType)).thenReturn(
-                ReservedAddress("0x2", "Memo", ethMemoAddressType)
-            )
+
             val assignedAddress = assignAddressServiceImpl.assignAddress(user, currency, chain)
-            Assertions.assertEquals(
+            assertThat(assignedAddress).isEqualTo(
                 listOf(
-                    AssignedAddress(
-                        user,
-                        "0x1",
-                        null,
-                        ethAddressType,
-                        mutableListOf(ethChain, bscChain)
-                    ),
-                    AssignedAddress(
-                        user,
-                        "0x2",
-                        "Memo",
-                        ethMemoAddressType,
-                        mutableListOf(bscChain)
-                    )
-                ), assignedAddress
-            )
-        }
-    }
-
-    @Test
-    fun givenNoReservedAddressAndUserWithNoAssignedAddress_whenAssignAddress_thenExcpetion() {
-        runBlocking {
-            val user = UUID.randomUUID().toString()
-            Mockito.`when`(
-                assignedAddressHandler.fetchAssignedAddresses(
-                    user,
-                    listOf(ethAddressType, ethMemoAddressType)
-                )
-            ).thenReturn(
-                emptyList()
-            )
-            Mockito.`when`(reservedAddressHandler.peekReservedAddress(ethAddressType)).thenReturn(null)
-
-            Assertions.assertThrows(RuntimeException::class.java) {
-                runBlocking {
-                    assignAddressServiceImpl.assignAddress(user, currency, chain)
-                }
-            }
-        }
-    }
-
-    @Test
-    fun givenReservedAddressAndUserOneAssignedAddress_whenAssignAddress_thenReservedAddressAssigned() {
-        runBlocking {
-            val user = UUID.randomUUID().toString()
-            Mockito.`when`(
-                assignedAddressHandler.fetchAssignedAddresses(
-                    user,
-                    listOf(ethAddressType, ethMemoAddressType)
-                )
-            ).thenReturn(
-                mutableListOf(
                     AssignedAddress(
                         user,
                         "0x1",
@@ -155,32 +101,62 @@ class AssignAddressServiceImplUnitTest {
                     )
                 )
             )
-            Mockito.`when`(reservedAddressHandler.peekReservedAddress(ethAddressType)).thenReturn(
-                ReservedAddress("0x1", null, ethAddressType)
+        }
+
+    @Test
+    fun givenNoReservedAddressAndUserWithNoAssignedAddress_whenAssignAddress_thenExcpetion(): Unit = runBlocking {
+        val user = UUID.randomUUID().toString()
+        coEvery {
+            assignedAddressHandler.fetchAssignedAddresses(
+                user,
+                listOf(ethAddressType, ethMemoAddressType)
             )
-            Mockito.`when`(reservedAddressHandler.peekReservedAddress(ethMemoAddressType)).thenReturn(
-                ReservedAddress("0x2", "Memo", ethMemoAddressType)
+        } returns emptyList()
+        coEvery { reservedAddressHandler.peekReservedAddress(ethAddressType) } returns null
+        coEvery { assignAddressServiceImpl.assignAddress(user, currency, chain) } throws RuntimeException()
+    }
+
+    @Test
+    fun givenReservedAddressAndUserOneAssignedAddress_whenAssignAddress_thenReservedAddressAssigned(): Unit =
+        runBlocking {
+            val user = UUID.randomUUID().toString()
+            coEvery {
+                assignedAddressHandler.fetchAssignedAddresses(
+                    eq(user),
+                    any()
+                )
+            } returns mutableListOf(
+                AssignedAddress(
+                    user,
+                    "0x1",
+                    null,
+                    ethAddressType,
+                    mutableListOf(ethChain)
+                )
             )
+            coEvery { reservedAddressHandler.peekReservedAddress(ethAddressType) } returns ReservedAddress(
+                "0x1",
+                null,
+                ethAddressType
+            )
+            coEvery { reservedAddressHandler.peekReservedAddress(ethMemoAddressType) } returns ReservedAddress(
+                "0x2",
+                "Memo",
+                ethMemoAddressType
+            )
+
             val assignedAddress = assignAddressServiceImpl.assignAddress(user, currency, chain)
-            Assertions.assertEquals(
+            assertThat(assignedAddress).isEqualTo(
                 listOf(
                     AssignedAddress(
                         user,
                         "0x1",
                         null,
                         ethAddressType,
-                        mutableListOf(ethChain, bscChain)
-                    ),
-                    AssignedAddress(
-                        user,
-                        "0x2",
-                        "Memo",
-                        ethMemoAddressType,
-                        mutableListOf(bscChain)
+                        mutableListOf(ethChain)
                     )
-                ), assignedAddress
+                )
             )
         }
-    }
 
 }
