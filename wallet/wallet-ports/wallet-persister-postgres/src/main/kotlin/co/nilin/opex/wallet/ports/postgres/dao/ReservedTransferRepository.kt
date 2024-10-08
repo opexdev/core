1a9078db4f84686b2288d9fc7c9a6269a055c4af
fatemeh imanipour <imanipour.fateme@gmail.com>
2024-10-08 17:00:05 +0330

    Handle off-chain withdraw request

--- wallet/wallet-ports/wallet-persister-postgres/src/main/kotlin/co/nilin/opex/wallet/ports/postgres/dao/ReservedTransferRepository.kt
+++ wallet/wallet-ports/wallet-persister-postgres/src/main/kotlin/co/nilin/opex/wallet/ports/postgres/dao/ReservedTransferRepository.kt
@@ -1,6 +1,7 @@
 package co.nilin.opex.wallet.ports.postgres.dao
 
 import co.nilin.opex.wallet.ports.postgres.model.ReservedTransferModel
+import org.springframework.data.r2dbc.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
 import org.springframework.stereotype.Repository
 import reactor.core.publisher.Flux
@@ -8,7 +9,12 @@ import reactor.core.publisher.Mono
 
 @Repository
 interface ReservedTransferRepository : ReactiveCrudRepository<ReservedTransferModel, Long> {
-    fun findByReserveNumber(reservedNumber:String): Mono<ReservedTransferModel>?
+    fun findByReserveNumber(reservedNumber: String): Mono<ReservedTransferModel>?
 
-    fun findByReservedTransfer():Flux<ReservedTransferModel>
+    @Query(
+        """
+        SELECT * from reserved_Transfer
+        """
+    )
+    fun findByReservedTransfer(): Flux<ReservedTransferModel>
 }
