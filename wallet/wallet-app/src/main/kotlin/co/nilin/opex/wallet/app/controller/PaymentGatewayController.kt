3523c28f156bf60f7b4bf42c27748aaf46f4bc8a
fatemeh imanipour <imanipour.fateme@gmail.com>
2024-12-01 20:43:50 +0330

    Record IPG deposits

--- wallet/wallet-app/src/main/kotlin/co/nilin/opex/wallet/app/controller/PaymentGatewayController.kt
+++ wallet/wallet-app/src/main/kotlin/co/nilin/opex/wallet/app/controller/PaymentGatewayController.kt
@@ -1,34 +1,39 @@
 package co.nilin.opex.wallet.app.controller
 
 import co.nilin.opex.common.OpexError
-import co.nilin.opex.wallet.app.dto.PaymentCurrency
 import co.nilin.opex.wallet.app.dto.PaymentDepositRequest
 import co.nilin.opex.wallet.app.dto.PaymentDepositResponse
+import co.nilin.opex.wallet.app.service.TraceDepositService
+import co.nilin.opex.wallet.core.inout.Deposit
 import co.nilin.opex.wallet.core.inout.TransferCommand
-import co.nilin.opex.wallet.core.model.Amount
-import co.nilin.opex.wallet.core.model.FetchCurrency
-import co.nilin.opex.wallet.core.model.TransferCategory
-import co.nilin.opex.wallet.core.model.WalletType
+import co.nilin.opex.wallet.core.inout.TransferMethod
+import co.nilin.opex.wallet.core.model.*
 import co.nilin.opex.wallet.core.spi.CurrencyServiceManager
 import co.nilin.opex.wallet.core.spi.TransferManager
 import co.nilin.opex.wallet.core.spi.WalletManager
 import co.nilin.opex.wallet.core.spi.WalletOwnerManager
+import org.springframework.transaction.annotation.Transactional
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 import java.math.BigDecimal
+import java.util.*
 
 @RestController
 @RequestMapping("/payment")
 class PaymentGatewayController(
-    val transferManager: TransferManager,
-    val currencyService: CurrencyServiceManager,
-    val walletManager: WalletManager,
-    val walletOwnerManager: WalletOwnerManager
+        val transferManager: TransferManager,
+        val currencyService: CurrencyServiceManager,
+        val walletManager: WalletManager,
+        val walletOwnerManager: WalletOwnerManager,
+        val traceDepositService: TraceDepositService
+
 ) {
 
+    //todo refactor
     @PostMapping("/internal/deposit")
+    @Transactional
     suspend fun paymentDeposit(@RequestBody request: PaymentDepositRequest): PaymentDepositResponse {
         val receiverWalletType = WalletType.MAIN
 //        val convertedAmount = when (request.currency) {
@@ -37,35 +42,50 @@ class PaymentGatewayController(
 //        }
 
         val currency =
-            currencyService.fetchCurrency(FetchCurrency(symbol = request.currency.name)) ?: throw OpexError.CurrencyNotFound.exception()
+                currencyService.fetchCurrency(FetchCurrency(symbol = request.currency.name))
+                        ?: throw OpexError.CurrencyNotFound.exception()
         val sourceOwner = walletOwnerManager.findWalletOwner(walletOwnerManager.systemUuid)
-            ?: throw OpexError.WalletOwnerNotFound.exception()
+                ?: throw OpexError.WalletOwnerNotFound.exception()
         val sourceWallet = walletManager.findWalletByOwnerAndCurrencyAndType(sourceOwner, WalletType.MAIN, currency)
-            ?: walletManager.createWallet(sourceOwner, Amount(currency, BigDecimal.ZERO), currency, WalletType.MAIN)
+                ?: walletManager.createWallet(sourceOwner, Amount(currency, BigDecimal.ZERO), currency, WalletType.MAIN)
 
         val receiverOwner = walletOwnerManager.findWalletOwner(request.userId)
-            ?: walletOwnerManager.createWalletOwner(request.userId, "not set", "")
+                ?: walletOwnerManager.createWalletOwner(request.userId, "not set", "")
 
         val receiverWallet = walletManager.findWalletByOwnerAndCurrencyAndType(
-            receiverOwner,
-            receiverWalletType,
-            currency
+                receiverOwner,
+                receiverWalletType,
+                currency
         ) ?: walletManager.createWallet(
-            receiverOwner,
-            Amount(currency, BigDecimal.ZERO),
-            currency,
-            receiverWalletType
+                receiverOwner,
+                Amount(currency, BigDecimal.ZERO),
+                currency,
+                receiverWalletType
         )
 
+        var depositCommand = Deposit(
+                receiverOwner.uuid,
+                UUID.randomUUID().toString(),
+                currency.symbol,
+                request.amount,
+                note = request.description,
+                transactionRef = request.reference,
+                status = DepositStatus.DONE,
+                depositType = DepositType.OFF_CHAIN,
+                network = TransferMethod.IPG.name,
+                attachment = null
+        )
+        traceDepositService.saveDepositInNewTransaction(depositCommand)
+
         transferManager.transfer(
-            TransferCommand(
-                sourceWallet,
-                receiverWallet,
-                Amount(sourceWallet.currency, request.amount),
-                request.description,
-                request.reference,
-                TransferCategory.DEPOSIT
-            )
+                TransferCommand(
+                        sourceWallet,
+                        receiverWallet,
+                        Amount(sourceWallet.currency, request.amount),
+                        request.description,
+                        request.reference,
+                        TransferCategory.DEPOSIT
+                )
         )
 
         return PaymentDepositResponse(true)
