f9c95db03aa2580b5e2fef558b7949ada5365ceb
Peyman <peyman.sepehrad@gmail.com>
2025-02-17 13:26:25 +0330

    Update spring version for api

--- api/api-app/src/main/kotlin/co/nilin/opex/api/app/config/InitializeService.kt
+++ api/api-app/src/main/kotlin/co/nilin/opex/api/app/config/InitializeService.kt
@@ -3,12 +3,12 @@ package co.nilin.opex.api.app.config
 import co.nilin.opex.api.ports.postgres.dao.SymbolMapRepository
 import co.nilin.opex.api.ports.postgres.model.SymbolMapModel
 import co.nilin.opex.utility.preferences.Preferences
+import jakarta.annotation.PostConstruct
 import kotlinx.coroutines.reactor.awaitSingleOrNull
 import kotlinx.coroutines.runBlocking
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.DependsOn
 import org.springframework.stereotype.Component
-import javax.annotation.PostConstruct
 
 @Component
 @DependsOn("postgresConfig")
--- api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/util/SecurityExtension.kt
+++ api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/util/SecurityExtension.kt
@@ -1,6 +1,5 @@
 package co.nilin.opex.api.ports.opex.util
 
-import com.nimbusds.jose.shaded.json.JSONArray
 import org.springframework.security.core.context.SecurityContext
 import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken
 
@@ -10,10 +9,4 @@ fun SecurityContext.jwtAuthentication(): JwtAuthenticationToken {
 
 fun JwtAuthenticationToken.tokenValue(): String {
     return this.token.tokenValue
-}
-
-fun JwtAuthenticationToken.roles(): List<String> {
-    val list = arrayListOf<String>()
-    (token.claims["roles"] as JSONArray?)?.forEach { list.add(it as String) }
-    return list
 }
