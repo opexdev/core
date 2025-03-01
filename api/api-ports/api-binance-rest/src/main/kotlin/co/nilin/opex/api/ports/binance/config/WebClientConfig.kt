2bbe460688d86a3ed897fd06db052c55c1d8d6c4
Peyman <peyman.sepehrad@gmail.com>
2025-03-01 17:07:39 +0330

    Modify webclient config

--- api/api-ports/api-binance-rest/src/main/kotlin/co/nilin/opex/api/ports/binance/config/WebClientConfig.kt
+++ api/api-ports/api-binance-rest/src/main/kotlin/co/nilin/opex/api/ports/binance/config/WebClientConfig.kt
@@ -23,16 +23,14 @@ class WebClientConfig {
     fun webClient(webclientBuilder: WebClient.Builder): WebClient {
         val cp = ConnectionProvider.builder("apiBinanceWebclientConnectionPool")
             .maxConnectionPools(500)
-            .maxIdleTime(Duration.ofSeconds(20))
-            .maxLifeTime(Duration.ofSeconds(60))
-            .pendingAcquireTimeout(Duration.ofSeconds(60))
-            .evictInBackground(Duration.ofSeconds(120))
+            .maxIdleTime(Duration.ofSeconds(10))
+            .maxLifeTime(Duration.ofSeconds(5))
+            .pendingAcquireTimeout(Duration.ofSeconds(5))
+            .evictInBackground(Duration.ofSeconds(30))
             .build()
         val client = HttpClient.create(cp)
-            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 20000)
-            .responseTimeout(Duration.ofSeconds(20))
-            .keepAlive(true)
-            .compress(true)
+            .option(ChannelOption.CONNECT_TIMEOUT_MILLIS, 5000)
+            .responseTimeout(Duration.ofSeconds(5))
         return webclientBuilder.clientConnector(ReactorClientHttpConnector(client)).build()
     }
 }
