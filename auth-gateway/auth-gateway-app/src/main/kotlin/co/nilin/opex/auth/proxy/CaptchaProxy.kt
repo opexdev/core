bbca8486a770a45a8f084fdb0127b9848b11c6d0
Amir Rajabi <amirrajabi02181@gmail.com>
2025-05-18 16:43:04 +0330

    add captcha enablement

--- auth-gateway/auth-gateway-app/src/main/kotlin/co/nilin/opex/auth/proxy/CaptchaProxy.kt
+++ auth-gateway/auth-gateway-app/src/main/kotlin/co/nilin/opex/auth/proxy/CaptchaProxy.kt
@@ -4,6 +4,7 @@ import co.nilin.opex.auth.model.CaptchaType
 import co.nilin.opex.common.OpexError
 import kotlinx.coroutines.reactive.awaitFirstOrNull
 import org.springframework.beans.factory.annotation.Qualifier
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
@@ -12,21 +13,25 @@ import org.springframework.web.reactive.function.client.WebClient
 import reactor.core.publisher.Mono
 
 @Component
-class CaptchaProxy(@Qualifier("captchaWebClient") private val webClient: WebClient) {
+class CaptchaProxy(
+    @Value("\${captcha.enabled}") private val captchaEnabled: Boolean,
+    @Qualifier("captchaWebClient") private val webClient: WebClient,
+) {
 
     suspend fun validateCaptcha(proof: String, type: CaptchaType) {
-        val statusCode = webClient.get().uri("/verify") {
-            it.queryParam("type", type)
-            it.queryParam("proof", proof)
-            it.build()
-        }.accept(MediaType.APPLICATION_JSON)
-            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
-            .exchangeToMono { response -> Mono.just(response.statusCode()) }.awaitFirstOrNull()
+        if (captchaEnabled) {
+            val statusCode = webClient.get().uri("/verify") {
+                it.queryParam("type", type)
+                it.queryParam("proof", proof)
+                it.build()
+            }.accept(MediaType.APPLICATION_JSON).header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .exchangeToMono { response -> Mono.just(response.statusCode()) }.awaitFirstOrNull()
 
-        when (statusCode) {
-            HttpStatus.NO_CONTENT -> return
-            HttpStatus.BAD_REQUEST -> throw OpexError.InvalidCaptcha.exception()
-            else -> throw OpexError.BadRequest.exception("Error in verify captcha")
+            when (statusCode) {
+                HttpStatus.NO_CONTENT -> return
+                HttpStatus.BAD_REQUEST -> throw OpexError.InvalidCaptcha.exception()
+                else -> throw OpexError.BadRequest.exception("Error in verify captcha")
+            }
         }
     }
 }
