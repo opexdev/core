41596a0595d32e43e43a879d198060336249306a
Peyman <peyman.sepehrad@gmail.com>
2025-07-23 15:32:51 +0330

    Fix email otp

--- otp/otp-app/src/main/kotlin/co/nilin/opex/otp/app/service/message/EmailSender.kt
+++ otp/otp-app/src/main/kotlin/co/nilin/opex/otp/app/service/message/EmailSender.kt
@@ -1,51 +1,55 @@
 package co.nilin.opex.otp.app.service.message
 
 import co.nilin.opex.common.utils.LoggerDelegate
-import jakarta.mail.Authenticator
 import jakarta.mail.Message
-import jakarta.mail.PasswordAuthentication
 import jakarta.mail.Session
-import jakarta.mail.Transport
 import jakarta.mail.internet.InternetAddress
 import jakarta.mail.internet.MimeMessage
-import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Component
-import java.util.*
 
 @Component
 class EmailSender(
     @Value("\${otp.email.host}")
     private val host: String,
+    @Value("\${otp.email.port}")
+    private val port: String,
     @Value("\${otp.email.username}")
     private val username: String,
     @Value("\${otp.email.password}")
-    private val password: String
+    private val password: String,
+    @Value("\${otp.email.from}")
+    private val from: String
 ) : MessageSender {
 
     private val logger by LoggerDelegate()
 
     override suspend fun send(receiver: String, message: String, metadata: Map<String, Any>): Boolean {
         val subject = "Your otp code"
-        val props = Properties().apply {
-            put("mail.smtp.host", host)
-            put("mail.smtp.port", "2525")
-            put("mail.smtp.auth", "true")
-            put("mail.smtp.username", username)
-        }
 
-        val auth = object : Authenticator() {
-            override fun getPasswordAuthentication() = PasswordAuthentication(username, password)
-        }
-        val session = Session.getInstance(props, auth)
+        val properties = System.getProperties()
+        properties.setProperty("mail.smtp.host", host)
+        properties["mail.smtp.port"] = port
+        properties["mail.smtp.auth"] = "true"
+        properties["mail.smtp.starttls.enable"] = "true"
+        properties["mail.smtp.from"] = from
+        properties["mail.smtp.ssl.protocols"] = "TLSv1.2"
+        val session = Session.getDefaultInstance(properties)
+
         return try {
-            val emailMessage = MimeMessage(session).apply {
-                setFrom(InternetAddress(username))
+            val msg = MimeMessage(session).apply {
                 setSubject(subject)
-                setRecipient(Message.RecipientType.TO, InternetAddress(receiver))
+                setFrom(InternetAddress(this@EmailSender.from))
+                addRecipient(Message.RecipientType.TO, InternetAddress(receiver))
                 setContent(message, "text/html; charset=utf-8")
             }
-            Transport.send(emailMessage)
+
+            with(session.getTransport("smtp")) {
+                connect(host, port.toInt(), username, password)
+                sendMessage(msg, msg.allRecipients)
+                close()
+            }
+
             logger.info("Successfully sent email message")
             true
         } catch (e: Exception) {
