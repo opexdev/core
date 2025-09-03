c6e7c917f961f2dd22ed9e01c0228378b6f07f21
Amir Rajabi <34955519+AmirRajabii@users.noreply.github.com>
2025-09-03 15:36:03 +0330

    Add assign address api to API module (#586)

--- api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/controller/WalletController.kt
+++ api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/controller/WalletController.kt
@@ -1,22 +1,26 @@
 package co.nilin.opex.api.ports.opex.controller
 
+import co.nilin.opex.api.core.inout.AssignAddressRequest
 import co.nilin.opex.api.core.inout.OwnerLimitsResponse
+import co.nilin.opex.api.core.spi.BlockchainGatewayProxy
 import co.nilin.opex.api.core.spi.WalletProxy
 import co.nilin.opex.api.ports.opex.data.AssetResponse
+import co.nilin.opex.api.ports.opex.data.AssignAddressResponse
 import co.nilin.opex.api.ports.opex.util.jwtAuthentication
 import co.nilin.opex.api.ports.opex.util.tokenValue
+import co.nilin.opex.common.OpexError
 import org.springframework.security.core.annotation.CurrentSecurityContext
 import org.springframework.security.core.context.SecurityContext
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController("walletOpexController")
 @RequestMapping("/opex/v1/wallet")
 class WalletController(
     private val walletProxy: WalletProxy,
+    private val bcGatewayProxy: BlockchainGatewayProxy,
 ) {
+    data class GetAssignAddressRequest(val currency: String, val gatewayUuid: String)
+
 
     @GetMapping("/asset")
     fun getUserAssets(
@@ -44,5 +48,23 @@ class WalletController(
             securityContext.jwtAuthentication().name,
             securityContext.jwtAuthentication().tokenValue(),
         )
+    }
+
+    @GetMapping("/deposit/address")
+    fun assignAddress(
+        @RequestBody assignAddressRequest: GetAssignAddressRequest,
+        @CurrentSecurityContext securityContext: SecurityContext
+    ): AssignAddressResponse {
+
+        val response = bcGatewayProxy.assignAddress(
+            AssignAddressRequest(
+                securityContext.authentication.name,
+                assignAddressRequest.currency,
+                assignAddressRequest.gatewayUuid
+            )
+        )
+        val address = response?.addresses
+        if (address.isNullOrEmpty()) throw OpexError.InternalServerError.exception()
+        return AssignAddressResponse(address[0].address, assignAddressRequest.currency)
     }
 }
---
+++ api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/data/AssignAddressResponse.kt
@@ -0,0 +1,6 @@
+package co.nilin.opex.api.ports.opex.data
+
+data class AssignAddressResponse(
+    val address: String,
+    val coin: String
+)
