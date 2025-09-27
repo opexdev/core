1ec7c638fbd1b62cf01c45d3fb4af5b446a688f7
fatemeh imanipour <imanipour.fateme@gmail.com>
2025-09-27 13:45:11 +0330

    Update the property naming of auth-gateway

--- auth-gateway/auth-gateway-app/src/main/kotlin/co/nilin/opex/auth/config/CaptchaConfig.kt
+++ auth-gateway/auth-gateway-app/src/main/kotlin/co/nilin/opex/auth/config/CaptchaConfig.kt
@@ -4,7 +4,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.stereotype.Component
 
 @Component
-@ConfigurationProperties(prefix = "captcha")
+@ConfigurationProperties(prefix = "app.captcha")
 class CaptchaConfig {
     lateinit var url: String
 }
