2c2714a7790f7fdc5e57427cccc75b7e95bb492b
Amir Rajabi <amirrajabi02181@gmail.com>
2026-04-23 18:10:14 +0330

    Update LocalizationAdminController.kt

--- api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/controller/LocalizationAdminController.kt
+++ api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/controller/LocalizationAdminController.kt
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.*
 class LocalizationAdminController(
     private val walletProxy: WalletProxy,
 ) {
-
     @GetMapping("/currency/{currency}")
     suspend fun getCurrencyLocalizations(
         @CurrentSecurityContext securityContext: SecurityContext,
