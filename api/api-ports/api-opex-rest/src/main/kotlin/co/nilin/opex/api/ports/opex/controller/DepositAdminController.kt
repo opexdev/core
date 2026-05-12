cdaaf5117a618946d546963b21f50efa22e8031d
fatemeh imanipour <imanipour.fateme@gmail.com>
2026-05-12 13:24:21 +0330

    Update swagger data about deposit admin services

--- api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/controller/DepositAdminController.kt
+++ api/api-ports/api-opex-rest/src/main/kotlin/co/nilin/opex/api/ports/opex/controller/DepositAdminController.kt
@@ -22,18 +22,16 @@ import io.swagger.v3.oas.annotations.tags.Tag
 @RequestMapping("/opex/v1/admin/deposit")
 @Tag(name = "Deposit Admin", description = "Admin manual deposit operations.")
 class DepositAdminController(
-    private val walletProxy: WalletProxy,
+    private val walletProxy: WalletProxy
 ) {
     @PostMapping("/manually/{amount}_{symbol}/{receiverUuid}")
     @Operation(
         summary = "Deposit manually",
-        description = """
-    POST /opex/v1/admin/deposit/manually/{amount}_{symbol}/{receiverUuid}.
-    Bearer admin-token required. Required authority: ROLE_admin.
-            """,
+        description = """POST /opex/v1/admin/deposit/manually/{amount}_{symbol}/{receiverUuid}.
+Security: Bearer admin-token required. Required authority: ROLE_admin.""",
         security = [SecurityRequirement(name = "bearerAuth")],
         responses = [
-            ApiResponse(responseCode = "200", description = "Successful response.", content = [Content(mediaType = "application/json", schema = Schema(implementation = TransferResult::class))]),
+            ApiResponse(responseCode = "200", description = "Response body: See schema.", content = [Content(mediaType = "application/json", schema = Schema(implementation = TransferResult::class))]),
             ApiResponse(responseCode = "401", description = "Unauthorized. Bearer token is missing, invalid, or expired. No response body.", content = [Content()]),
             ApiResponse(responseCode = "403", description = "Forbidden. Required authority is missing: ROLE_admin. No response body.", content = [Content()])
         ]
